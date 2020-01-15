cc6f5f41de4adf10bef07d9226dcf99f0fc75ed7
Bluebada <wxg1297@gmail.com>
2020-01-15 16:38:05 +0900

    Add User Remarks

--- dev_koin/User/AddUserView.swift
+++ dev_koin/User/AddUserView.swift
@@ -11,32 +11,46 @@ import PKHUD
 
 
 struct AddUserView: View {
+    // 이메일 정보를 담는 변수
     @State var login_email: String = ""
+    // 패스워드 정보를 담는 변수
     @State var login_password: String = ""
+    // 패스워드가 일치하는지 확인하는 변수
     @State var login_valid_password: String = ""
+    // 로그인 페이지로 돌아갈 때 사용하는 객체
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    // 회원가입 성공 시 Alert를 띄우는 목적의 변수
     @State private var showingSuccessAlert = false
-    
+    // 실패시, 에러 HUD를 띄울 때의 String 변수
     @State var errorText = ""
-    
+    // 약관 동의 여부를 확인하는 객체
     @EnvironmentObject var property: AddUserProperty
+    // 유저 정보가 담겨있는 객체
     @EnvironmentObject var settings: UserSettings
     
-    
+    // 회원가입 기능을 제공하는 함수
     func check_register(email: String, password: String, valid_password: String) {
+        // 에러 HUD를 위한 임의의 뷰 객체
         let uiview = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+        // 에러 HUD 내에서의 에러 문자 뷰 객체
         let yourLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
         yourLabel.center = CGPoint(x: uiview.frame.size.width  / 2,
         y: uiview.frame.size.height / 2)
         yourLabel.textAlignment = .center
         
+        // settings 내의 패스워드 일치 여부를 확인하고
         if settings.check_valid_password(password: password, valid_password: valid_password) {
+            // 이용약관 동의 여부를 모두 확인한 다음
             if self.property.personality_checked && self.property.koin_checked {
+                // 회원가입 정보를 서버로 보내준 후에
                 self.settings.register_session(email: email, password: password) { result in
-                    if result {
+                    if result { // 성공하면
+                        // 성공했다고 알려주는 Alert을 열고
                         self.showingSuccessAlert = true
-                    } else {
+                    } else { // 아니면
+                        // 에러 문자로 회원 가입에 실패했다고 알려주고
                         self.errorText = "회원가입에 실패하였습니다."
+                        // 에러 HUD를 보여준다.
                         yourLabel.text = self.errorText
                         uiview.addSubview(yourLabel)
                         PKHUD.sharedHUD.contentView = uiview
@@ -44,16 +58,20 @@ struct AddUserView: View {
                         PKHUD.sharedHUD.hide(afterDelay: 1.0)
                     }
                 }
-            } else {
+            } else { // 동의되지 않은 약관이 있으면
+                // 동의되지 않은 약관이 있다고 알려주고
                 self.errorText = "동의되지 않은 약관이 있습니다."
+                // 에러 HUD를 보여준다.
                 yourLabel.text = self.errorText
                 uiview.addSubview(yourLabel)
                 PKHUD.sharedHUD.contentView = uiview
                 PKHUD.sharedHUD.show()
                 PKHUD.sharedHUD.hide(afterDelay: 1.0)
             }
-        } else {
+        } else { // 비밀번호가 다르다면
+            // 비밀번호가 다르다고 알려주고
             self.errorText = "비밀번호가 다릅니다."
+            // 에러 HUD를 보여준다.
             yourLabel.text = self.errorText
             uiview.addSubview(yourLabel)
             PKHUD.sharedHUD.contentView = uiview
@@ -108,6 +126,8 @@ struct AddUserView: View {
                 .underline()
                 Spacer()
                 Button(action: {
+                    // 개인정보 이용약관이 체크되어있지 않으면 체크해주고
+                    // 체크되어있으면, 체크를 해제해준다.
                     self.property.personality_checked.toggle()
                 }) {
                     Image(systemName: self.property.personality_checked ? "checkmark.circle.fill" : "circle.fill")
@@ -126,7 +146,8 @@ struct AddUserView: View {
                 
                 Spacer()
                 Button(action: {
-                    
+                    // 코인 이용약관이 체크되어있지 않으면 체크해주고
+                    // 체크되어있으면, 체크를 해제해준다.
                     self.property.koin_checked.toggle()
                 }) {
                     Image(systemName: self.property.koin_checked ? "checkmark.circle.fill" : "circle.fill")
@@ -168,10 +189,14 @@ struct AddUserView: View {
             
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding([.leading, .trailing], CGFloat(50))
+            //showingSuccessAlert이 true일 때 Alert을 표시한다.
         .alert(isPresented: $showingSuccessAlert) {
+            // 이메일을 확인해보라는 Alert을 띄운 다음
                 Alert(title: Text("이메일 확인"), message: Text("회원 가입을 완료하시려면 메일을 확인해보세요."), dismissButton: .default(Text("돌아가기")) {
+                    // 돌아가기 버튼을 누르면 Alert은 꺼지고
                     self.showingSuccessAlert = false
-                    self.presentationMode.wrappedValue.dismiss()
+                   // 로그인 페이지로 돌아간다.
+                self.presentationMode.wrappedValue.dismiss()
                     })
         }
         
