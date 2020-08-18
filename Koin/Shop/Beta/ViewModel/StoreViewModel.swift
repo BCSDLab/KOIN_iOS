90ac2e98ce5c79248cae5e0fe71eca8f1d27e344
Bluebada <wxg1297@gmail.com>
2020-08-19 00:23:52 +0900

    store MVVM 패턴

--- Koin/Shop/Beta/ViewModel/StoreViewModel.swift
+++ Koin/Shop/Beta/ViewModel/StoreViewModel.swift
@@ -11,16 +11,58 @@ import Combine
 
 class StoreViewModel: ObservableObject {
     @Published var data = [StoreRowViewModel]()
+    
+    @Published var previousShowList = [StoreRowViewModel]()
+    
+    @Published var showList = [StoreRowViewModel]()
+    
     let storeFetcher: StoreFetcher = StoreFetcher()
 
     var result = PassthroughSubject<[StoreRowViewModel], Never>()
+    
+    var changeCategory = PassthroughSubject<String, Never>()
 
-    @Published var category: String = ""
+    @Published var category: String = "" {
+        didSet {
+            if(!oldValue.isEmpty) {
+                self.revert()
+                if(!category.isEmpty) {
+                    self.filter()
+                }
+                changeCategory.send(category)
+            } else {
+                self.filter()
+                changeCategory.send(category)
+            }
+        }
+    }
 
     private var disposables = Set<AnyCancellable>()
 
     deinit {
         disposables.removeAll()
+    }
+    
+    func filter() {
+         
+        self.showList.removeAll { d in
+            return d.category != self.category
+        }
+        
+        self.result.send(self.showList)
+    }
+    
+    func revert() {
+        
+        self.data.forEach { d in
+            if (!self.showList.contains(d)) {
+                self.showList.append(d)
+            }
+        }
+        
+        self.showList = self.showList.sorted(by: {$0.id < $1.id})
+        
+        self.result.send(self.showList)
     }
 
     func load() {
@@ -40,7 +82,16 @@ class StoreViewModel: ObservableObject {
                         break
                     }
                 }, receiveValue: { stores in
+                    /*var filteredResult: [StoreRowViewModel] = []
+                    
+                    stores.forEach { s in
+                        if(s.category == self.category || self.category == "") {
+                            filteredResult.append(s)
+                        }
+                    }*/
+                    
                     self.data = stores
+                    self.showList = stores
                     self.result.send(stores)
                 })
                 .store(in: &disposables)
