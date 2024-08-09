b53169fa2b612dc9455d9b2421b9eb5017985b4b
Ju-Min-Kyung <alsrud7767@naver.com>
2024-08-09 19:22:38 +0900

    chore: MockAnalyticService 없앰

--- Koin/Data/Repository/GA4AnalyticsRepository.swift
+++ Koin/Data/Repository/GA4AnalyticsRepository.swift
@@ -16,8 +16,5 @@ final class GA4AnalyticsRepository: AnalyticsRepository {
     
     func logEvent(label: EventLabelType, category: EventParameter.EventCategory, value: Any, previousPage: String? = nil, currentPage: String? = nil, durationTime: String? = nil, eventLabelNeededDuration: EventParameter.EventLabelNeededDuration? = nil) {
         service.logEvent(label: label, category: category, value: value, previousPage: previousPage, currentPage: currentPage, durationTime: durationTime, eventLabelNeededDuration: eventLabelNeededDuration)
-        
-        let mockService = MockAnalyticsService()
-        mockService.logEvent(label: label, category: category, value: value, previousPage: previousPage, currentPage: currentPage, durationTime: durationTime, eventLabelNeededDuration: eventLabelNeededDuration)
     }
 }
