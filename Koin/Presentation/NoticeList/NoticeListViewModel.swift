34c4e2820c9fc728d91cb7837c9ab8e0d60084c4
Ju-Min-Kyung <alsrud7767@naver.com>
2024-08-18 13:43:34 +0900

    feat: 공지사항 테이블뷰 로직 연결

--- Koin/Presentation/NoticeList/NoticeListViewModel.swift
+++ Koin/Presentation/NoticeList/NoticeListViewModel.swift
@@ -12,19 +12,38 @@ final class NoticeListViewModel: ViewModelProtocol {
     
     enum Input {
         case changeBoard(NoticeListType)
+        case changePage(Int)
     }
     enum Output {
         case updateBoard([NoticeArticleDTO], NoticeListType)
+        case updatePageList(NoticeListPages)
     }
     
     private let outputSubject = PassthroughSubject<Output, Never>()
     private var subscriptions: Set<AnyCancellable> = []
+    private let fetchNoticeArticlesUseCase: FetchNoticeArticlesUseCase
+    private var noticeListType: NoticeListType = .자유게시판 {
+        didSet {
+            getNoticeInfo(page: 1)
+        }
+    }
+    private var keyWord: String? = nil {
+        didSet {
+            getNoticeInfo(page: 0)
+        }
+    }
+    
+    init(fetchNoticeArticlesUseCase: FetchNoticeArticlesUseCase) {
+        self.fetchNoticeArticlesUseCase = fetchNoticeArticlesUseCase
+    }
     
     func transform(with input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
         input.sink { [weak self] input in
             switch input {
             case let .changeBoard(noticeListType):
                 self?.changeBoard(noticeListType: noticeListType)
+            case let .changePage(page):
+                self?.getNoticeInfo(page: page)
             }
         }.store(in: &subscriptions)
         return outputSubject.eraseToAnyPublisher()
@@ -33,8 +52,20 @@ final class NoticeListViewModel: ViewModelProtocol {
 
 extension NoticeListViewModel {
     private func changeBoard(noticeListType: NoticeListType) {
-        let testArticles = [NoticeArticleDTO(id: 0, boardID: 0, title: "aweawfe", content: "asdfaswe", nickname: "eafefs", hit: 2, createdAt: "aweaf", updatedAt: "awefwa")]
-        outputSubject.send(.updateBoard(testArticles, noticeListType))
+        self.noticeListType = noticeListType
+    }
+    
+    private func getNoticeInfo(page: Int) {
+        fetchNoticeArticlesUseCase.fetchArticles(boardId: noticeListType.rawValue, keyWord: keyWord, page: page).sink(receiveCompletion: { completion in
+            if case let .failure(error) = completion {
+                Log.make().error("\(error)")
+            }
+        }, receiveValue: { [weak self] articleInfo in
+            print(articleInfo)
+            guard let self = self else { return }
+            self.outputSubject.send(.updateBoard(articleInfo.articles, self.noticeListType))
+            self.outputSubject.send(.updatePageList(articleInfo.pages))
+        }).store(in: &subscriptions)
     }
 }
 
