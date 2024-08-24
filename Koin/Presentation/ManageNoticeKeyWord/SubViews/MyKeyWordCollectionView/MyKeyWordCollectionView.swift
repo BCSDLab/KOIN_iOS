675fcf460ef44ab8dea5f71ae4f43eabd4c4d0af
Ju-Min-Kyung <alsrud7767@naver.com>
2024-08-24 16:59:40 +0900

    design: 키워드 관리 화면 UI 구현

---
+++ Koin/Presentation/ManageNoticeKeyWord/SubViews/MyKeyWordCollectionView/MyKeyWordCollectionView.swift
@@ -0,0 +1,66 @@
+//
+//  MyKeyWordCollectionView.swift
+//  koin
+//
+//  Created by JOOMINKYUNG on 8/24/24.
+//
+
+import Combine
+import UIKit
+
+final class MyKeyWordCollectionView: UICollectionView, UICollectionViewDataSource {
+    //MARK: - Properties
+    private var myKeyWordList: [NoticeKeyWordDTO] = [NoticeKeyWordDTO(id: 1, keyWord: "교환학생"),
+                                                     NoticeKeyWordDTO(id: 1, keyWord: "교환학생"),
+                                                     NoticeKeyWordDTO(id: 1, keyWord: "교환학생"),
+                                                     NoticeKeyWordDTO(id: 1, keyWord: "교환학생"),
+                                                     NoticeKeyWordDTO(id: 1, keyWord: "교환학생")]
+    
+    //MARK: - Initialization
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        register(MyKeyWordCollectionViewCell.self, forCellWithReuseIdentifier: MyKeyWordCollectionViewCell.identifier)
+        dataSource = self
+        delegate = self
+        contentInset = UIEdgeInsets(top: 12, left: 24, bottom: 12, right: 24)
+    }
+ 
+}
+
+extension MyKeyWordCollectionView {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myKeyWordList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyKeyWordCollectionViewCell.identifier, for: indexPath) as? MyKeyWordCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(keyWord: myKeyWordList[indexPath.item].keyWord)
+        return cell
+    }
+}
+
+extension MyKeyWordCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let label = UILabel()
+        label.text = myKeyWordList[indexPath.row].keyWord
+        label.font = .appFont(.pretendardMedium, size: 14)
+        let size = label.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude, height: 34))
+        return CGSize(width: size.width + 50, height: 34)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}
+
