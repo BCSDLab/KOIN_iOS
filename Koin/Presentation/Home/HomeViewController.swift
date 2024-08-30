7f41127954164b310765647dc771a82814cc412e
Ju-Min-Kyung <alsrud7767@naver.com>
2024-08-31 02:24:37 +0900

    feat: 더보기 버튼을 눌렀을 때 공지사항 목록 페이지로 가는 기능 구현

--- Koin/Presentation/Home/HomeViewController.swift
+++ Koin/Presentation/Home/HomeViewController.swift
@@ -307,6 +307,14 @@ final class HomeViewController: UIViewController, CollectionViewDelegate {
 }
 
 extension HomeViewController {
+    @objc private func tapGoNoticePageButton() {
+        let service = DefaultNoticeService()
+        let repository = DefaultNoticeListRepository(service: service)
+        let fetchArticleListUseCase = DefaultFetchNoticeArticlesUseCase(noticeListRepository: repository)
+        let viewModel = NoticeListViewModel(fetchNoticeArticlesUseCase: fetchArticleListUseCase)
+        let noticeListViewController = NoticeListViewController(viewModel: viewModel)
+        navigationController?.pushViewController(noticeListViewController, animated: true)
+    }
     
     private func checkAndShowTooltip() {
         let hasShownImage = UserDefaults.standard.bool(forKey: "hasShownTooltip")
@@ -633,6 +641,7 @@ extension HomeViewController {
         scrollView.alwaysBounceVertical = true
         refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
         noticePageControl.addTarget(self, action: #selector(pageControlDidChange), for: .valueChanged)
+        goNoticePageButton.addTarget(self, action: #selector(tapGoNoticePageButton), for: .touchUpInside)
         scrollView.refreshControl = refreshControl
         self.view.backgroundColor = .systemBackground
     }
