8d22b16af0e92a85be954ac0dbccf97dc6217a5c
Ju-Min-Kyung <alsrud7767@naver.com>
2024-08-31 21:23:09 +0900

    feat: 공지사항 이미지 확대 구현

--- Koin/Core/Extensions/String+.swift
+++ Koin/Core/Extensions/String+.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Kingfisher
 import SwiftSoup
 
 extension String {
@@ -23,53 +24,78 @@ extension String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyMMdd"
         return dateFormatter.date(from: self)
+    }
 
-    func extractFromHtmlTag(htmlTag: String) -> NSAttributedString? {
+    func modifyFontInHtml() -> String? {
         do {
             let document = try SwiftSoup.parse(self)
-            try document.select("img").remove()
             try document.select("br").remove()
-            var emptyPCount = 0 
-            let paragraphs = try document.select("p")
             
+            let paragraphs = try document.select("p")
+            let bTags = try document.select("b")
+            let iTags = try document.select("i")
+            let hTags = try document.select("h1, h2, h3, h4, h5, h6")
+            let spans = try document.select("span")
+            let ulTags = try document.select("ul")
+            let olTags = try document.select("ol")
+            let liTags = try document.select("li")
+            
+            var emptyPCount = 0
             for paragraph in paragraphs {
                 let paragraphText = try paragraph.text()
-                if try paragraphText.isEmpty || paragraph.html().contains("br") {
+                if paragraphText.isEmpty {
                     emptyPCount += 1
                 } else {
                     emptyPCount = 0
                 }
-               
+                
                 if emptyPCount > 1 {
                     try paragraph.remove()
                 }
+                
+                if let img = try paragraph.select("img").first() {
+                    try paragraph.append("<p></p>")
+                }
             }
-           
-            let changedHtml = try document.body()?.html() ?? ""
-          
-            let data = Data(changedHtml.utf8)
-            let options: [NSAttributedString.DocumentReadingOptionKey : Any] = [
-                .documentType: NSAttributedString.DocumentType.html,
-                .characterEncoding: String.Encoding.utf8.rawValue
-            ]
-            let attributedString = try NSMutableAttributedString(data: data, options: options, documentAttributes: nil)
-       
-            let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.lineHeightMultiple = 1.5
-            attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
-            
-            let bTag = try NSRegularExpression(pattern: "<b>(.*?)</b>", options: [])
-            let nsStringValue = attributedString.string as NSString
-            let results = bTag.matches(in: changedHtml, options: [], range: NSRange(location: 0, length: nsStringValue.length))
-            
-            attributedString.addAttribute(.font, value: regularFont, range: NSRange(location: 0, length: attributedString.length))
-            
-            for result in results {
-                let matchRange = result.range(at: 1)
-                attributedString.addAttribute(.font, value: boldFont, range: matchRange)
+        
+            let allTags: [Elements] = [paragraphs, bTags, iTags, hTags, spans, ulTags, olTags, liTags]
+            for elements in allTags {
+                for element in elements {
+                    try element.attr("style", "font-family: 'Pretandard-Medium', sans-serif; font-size: 16px;")
+                }
             }
             
-            return attributedString
+            for element in bTags {
+                try element.attr("style", "font-family: 'Pretandard-Bold', sans-serif; font-size: 16px;")
+            }
+            
+            let setHeightUsingCSS = """
+                <head>
+                    <style type="text/css">
+                        img {
+                            max-height: 100%;
+                            max-width: 317px !important;
+                            width: auto;
+                            height: auto;
+                            display: block;
+                            margin-bottom: 20px; /* Space below the image */
+                        }
+                        .txc-image {
+                            text-align: center; /* Center the image */
+                        }
+                        .bc-s-post-ctnt-area {
+                            padding: 20px; /* Optional padding */
+                            border-top: 1px solid transparent; /* Prevent margin collapse */
+                        }
+                    </style>
+                </head>
+                <body class="center">
+                    <div class="bc-s-post-ctnt-area">
+                        \(try document.body()?.html() ?? "")
+                    </div>
+                </body>
+                """
+            return setHeightUsingCSS
         } catch {
             print("Error processing HTML: \(error)")
             return nil
@@ -86,7 +112,59 @@ extension String {
         } catch {
             return nil
         }
->>>>>>> b0cb878 (feat: 공지사항 상세 뷰 데이터 연결)
+    }
+    
+    func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        KingfisherManager.shared.retrieveImage(with: url) { result in
+            switch result {
+            case .success(let value):
+                completion(value.image)
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
+                completion(nil)
+            }
+        }
+    }
+    
+    func convertToAttributedFromHTML() -> NSAttributedString? {
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+  
+        guard let data = self.data(using: .utf8) else { return nil }
+        
+        do {
+            let attributedString = try NSAttributedString(data: data, options: options, documentAttributes: nil)
+            let text = NSMutableAttributedString(attributedString: attributedString)
+           
+            let docs: Document = try SwiftSoup.parse(self)
+            let imgElements: Elements = try docs.select("img")
+            
+            imgElements.forEach { element in
+                let imageSrc = (try? element.attr("src")) ?? ""
+                
+                let range = NSMakeRange(0, text.length)
+                
+                text.enumerateAttribute(NSAttributedString.Key.attachment, in: range, options: []) { (value, _, _) in
+                    if let attachment = value as? NSTextAttachment {
+                        imageSrc.loadImage(from: imageSrc, completion: { image in
+                            attachment.image = image
+                        })
+                    }
+                }
+            }
+            
+            return text
+        } catch {
+            print("Fail: \(error.localizedDescription)")
+            return nil
+        }
     }
     
     func toDateFromYYYYMMDD() -> Date? {
