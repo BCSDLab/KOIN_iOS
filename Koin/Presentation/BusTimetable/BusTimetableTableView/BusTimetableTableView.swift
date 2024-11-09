9ddf4cb03f5b0036b0e4f0e8d11d55be60a9e46f
Ju-Min-Kyung <alsrud7767@naver.com>
2024-11-09 20:29:25 +0900

    add: 셔틀버스, 대성고속, 시내버스 tableViewCell 추가

---
+++ Koin/Presentation/BusTimetable/BusTimetableTableView/BusTimetableTableView.swift
@@ -0,0 +1,14 @@
+//
+//  BusTimetableTableView.swift
+//  koin
+//
+//  Created by JOOMINKYUNG on 11/9/24.
+//
+
+import Combine
+import UIKit
+
+final class BusTimetableTableView: UITableView {
+    
+}
+
