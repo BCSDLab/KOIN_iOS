0a124d78e5fd54a55ac084c562e1d56df16db5f9
Ju-Min-Kyung <alsrud7767@naver.com>
2024-11-22 01:07:16 +0900

    chore: 배포 후 7일 주기로 문구 바뀌게 수정

--- Koin/Domain/UseCase/NoticeList/FetchKeywordNoticePhraseUseCase.swift
+++ Koin/Domain/UseCase/NoticeList/FetchKeywordNoticePhraseUseCase.swift
@@ -13,12 +13,12 @@ protocol FetchKeywordNoticePhraseUseCase {
 
 final class DefaultFetchKeywordNoticePhraseUseCase: FetchKeywordNoticePhraseUseCase {
     func execute(date: Date) -> (String, String) {
-        return makeKeywordNoticePhrase()[0]
+        return makeKeywordNoticePhrase(date: date)
     }
     
-    private func makeKeywordNoticePhrase() -> [(String, String)] {
-        let titles = ["키워드가 포함된 공지가 업로드 되면\n가장 먼저 알림을 보내드려요!", "근로 공지, 놓치고 싶지 않다면?", "해외탐방 공지, 놓치고 싶지 않다면?", "해외탐방 공지, 놓치고 싶지 않다면?", "자취방 양도글, 가장 먼저 확인하고 싶을 때?"]
-        let subTitles = ["키워드 알림 설정 바로가기", "공지가 업로드 되면 바로 알려주는\n키워드 알림 설정하러가기", "공지가 업로드 되면 바로 알려주는\n키워드 알림 설정하러가기", "공지가 업로드 되면 바로 알려주는\n키워드 알림 설정하러가기"]
+    private func makeKeywordNoticePhrase(date: Date) -> (String, String) {
+        let titles = ["자취방 양도글, 가장 먼저 확인하고 싶을 때?", "키워드가 포함된 공지가 업로드 되면\n가장 먼저 알림을 보내드려요!", "근로 공지, 놓치고 싶지 않다면?", "해외탐방 공지, 놓치고 싶지 않다면?", "해외탐방 공지, 놓치고 싶지 않다면?"]
+        let subTitles = ["공지가 업로드 되면 바로 알려주는\n키워드 알림 설정하러가기", "키워드 알림 설정 바로가기", "공지가 업로드 되면 바로 알려주는\n키워드 알림 설정하러가기", "공지가 업로드 되면 바로 알려주는\n키워드 알림 설정하러가기"]
         var noticePhrases: [(String, String)] = []
         
         for (index, title) in titles.enumerated() {
@@ -26,7 +26,21 @@ final class DefaultFetchKeywordNoticePhraseUseCase: FetchKeywordNoticePhraseUseC
                 noticePhrases.append((title, subTitles[index]))
             }
         }
-        
-        return noticePhrases
+        let userDefaultsKey = "keywordBannerRegisteredAt"
+    
+        if let registeredDate = UserDefaults.standard.object(forKey: userDefaultsKey) as? Date {
+            let calendar = Calendar.current
+            if let daysDifference = calendar.dateComponents([.day], from: registeredDate, to: date).day {
+                print(registeredDate)
+                print(daysDifference)
+                
+                let difference = daysDifference % 7 == 0 ? daysDifference - 1 : daysDifference
+                let index = ((difference) / 7) % 4
+                return noticePhrases[index]
+            }
+        } else {
+            UserDefaults.standard.set(Date(), forKey: userDefaultsKey)
+        }
+        return noticePhrases[0]
     }
 }
