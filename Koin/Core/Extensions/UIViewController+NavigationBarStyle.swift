f960a57d7d567e6afd15b7263f49958374c6f519
KimNahun <rlaskgns0212@naver.com>
2024-12-03 23:27:49 +0900

    feat: 앱 전체에 전역적으로 쓰는 navigationbarstyle함수 추가

---
+++ Koin/Core/Extensions/UIViewController+NavigationBarStyle.swift
@@ -0,0 +1,43 @@
+//
+//  UIViewController+NavigationBarStyle.swift
+//  koin
+//
+//  Created by 김나훈 on 12/3/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    enum NavigationBarStyle {
+        case fill
+        case empty
+    }
+    
+    func configureNavigationBar(style: NavigationBarStyle) {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        
+        switch style {
+        case .fill:
+            appearance.backgroundColor = UIColor.appColor(.primary500)
+            appearance.titleTextAttributes = [
+                .foregroundColor: UIColor.appColor(.neutral0),
+                .font: UIFont.appFont(.pretendardMedium, size: 18)
+            ]
+            navigationItem.backButtonTitle = ""
+        case .empty:
+            appearance.backgroundColor = UIColor.appColor(.neutral0)
+            appearance.titleTextAttributes = [
+                .foregroundColor: UIColor.appColor(.neutral800),
+                .font: UIFont.appFont(.pretendardMedium, size: 18)
+            ]
+            navigationItem.backButtonTitle = ""
+        }
+        appearance.shadowColor = nil
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.tintColor = style == .fill ? UIColor.appColor(.neutral0) : UIColor.appColor(.neutral800)
+    }
+}
