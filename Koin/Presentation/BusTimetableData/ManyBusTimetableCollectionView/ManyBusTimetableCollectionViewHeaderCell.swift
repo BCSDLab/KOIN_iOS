381a6f216b2e728bf7688548176876ba81980632
Ju-Min-Kyung <alsrud7767@naver.com>
2024-12-07 12:13:32 +0900

    add: 스크롤되는 셔틀 시간표 디테일뷰 구현

---
+++ Koin/Presentation/BusTimetableData/ManyBusTimetableCollectionView/ManyBusTimetableCollectionViewHeaderCell.swift
@@ -0,0 +1,57 @@
+//
+//  ManyBusTimetableCollectionViewHeaderCell.swift
+//  koin
+//
+//  Created by JOOMINKYUNG on 12/7/24.
+//
+
+import SnapKit
+import Then
+import UIKit
+
+final class ManyBusTimetableCollectionViewHeaderCell: UICollectionViewCell {
+    static let reuseIdentifier = "manyBusTimetableCollectionViewHeaderCell"
+    
+    // MARK: - UI Components
+    private let busTimeNumberLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.font = .appFont(.pretendardRegular, size: 14)
+        $0.textColor = .appColor(.neutral600)
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(busTimeNumber: Int) {
+        busTimeNumberLabel.text = "\(busTimeNumber)회"
+    }
+}
+
+extension ManyBusTimetableCollectionViewHeaderCell {
+    private func setUpLayouts() {
+        [busTimeNumberLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setUpConstraints() {
+        busTimeNumberLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(22)
+        }
+    }
+    
+    private func configureView() {
+        setUpLayouts()
+        setUpConstraints()
+        contentView.backgroundColor = .appColor(.neutral100)
+    }
+}
+
