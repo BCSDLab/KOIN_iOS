1b2828e11fcd88ec83dce45109dfb28dab7f5c7f
KimNahun <rlaskgns0212@naver.com>
2024-12-11 22:55:09 +0900

    fix: 강의 색상 순서가 뒤바뀌던 버그 수정

--- Koin/Presentation/Timetable/TimetableViewController.swift
+++ Koin/Presentation/Timetable/TimetableViewController.swift
@@ -367,73 +367,69 @@ extension TimetableViewController {
         navigationController?.pushViewController(FrameListViewController(viewModel: viewModel), animated: true)
     }
     private func updateTimetable(lectureData: [LectureData]) {
-        // 현재 남아있는 LectureView들의 `info`를 비교하기 위해 사용
+        
         let existingLectureViews = containerView.subviews.compactMap { $0 as? LectureView }
-        
-        // 새로 추가된 LectureData와 기존 LectureView를 비교
         let existingLectureInfos = Set(existingLectureViews.map { $0.info })
-        let newLectureInfos = Set(lectureData)
         
-        // 추가된 강의 데이터 (새로운 데이터)
-        let addedLectures = newLectureInfos.subtracting(existingLectureInfos)
-        // 제거된 강의 데이터 (기존에 있던 데이터 중 새로 들어온 데이터에 없는 경우)
-        let removedLectureViews = existingLectureViews.filter { !newLectureInfos.contains($0.info) }
-        removedLectureViews.forEach { $0.removeFromSuperview() }
+        let addedLectures = lectureData.filter { !existingLectureInfos.contains($0) }
+        let removedLectureViews = existingLectureViews.filter { !lectureData.contains($0.info) }
         
-        // 이미 사용된 body 색상을 추적
-        var usedColors: Set<UIColor> = Set(existingLectureViews.compactMap { $0.backgroundColor })
+        var usedColors: [UIColor] = existingLectureViews.compactMap { $0.backgroundColor }
+        for lectureView in removedLectureViews {
+            if let removedColor = lectureView.backgroundColor {
+                usedColors.removeAll { $0 == removedColor }
+            }
+            lectureView.removeFromSuperview()
+        }
         
-        // 강의 ID와 할당된 색상 쌍을 저장
         var lectureColorMap: [Int: (UIColor, UIColor)] = [:]
-        
         for lectureView in existingLectureViews {
-            let lecture = lectureView.info // `info`가 Optional이 아니므로 바로 사용 가능
+            let lecture = lectureView.info
             if let bodyColor = lectureView.backgroundColor, let headerColor = lectureView.separateView.backgroundColor {
                 lectureColorMap[lecture.id] = (bodyColor, headerColor)
             }
         }
         
-        // 사용 가능한 색상 쌍에서 이미 사용된 것을 제외
-        var unusedColorPairs = timetableColors.compactMap { colorPair -> (UIColor, UIColor)? in
+        var unusedColorPairs: [(UIColor, UIColor)] = timetableColors.compactMap { colorPair in
             let bodyColor = UIColor.timetableColor(_name: colorPair.0)
             let headerColor = UIColor.timetableColor(_name: colorPair.1)
             return usedColors.contains(bodyColor) ? nil : (bodyColor, headerColor)
         }
         
-        // 새로운 강의 데이터를 추가
         for lecture in addedLectures {
-            let groupedByDay = Dictionary(grouping: lecture.classTime) { $0 / 100 } // 요일별로 그룹화
+            let groupedByDay = Dictionary(grouping: lecture.classTime) { $0 / 100 }
             
             for (day, times) in groupedByDay {
-                let separatedTimes = splitIntoContinuousRanges(times) // 연속된 시간을 나눔
+                let separatedTimes = splitIntoContinuousRanges(times)
                 
                 for range in separatedTimes {
                     if let firstTime = range.first {
-                        let width = Int(containerView.frame.width / 5) + 1 // 5열 기준
+                        let width = Int(containerView.frame.width / 5) + 1
                         let height = 35
                         
-                        // 색상 쌍을 가져옴 (기존 강의 색상이 있으면 동일한 색 사용)
+                        // 기존 강의 색상 재사용 또는 새 색상 할당
                         let colorPair: (UIColor, UIColor)
                         if let existingColor = lectureColorMap[lecture.id] {
                             colorPair = existingColor
                         } else {
-                            colorPair = unusedColorPairs.isEmpty ? (UIColor.appColor(.neutral300), UIColor.appColor(.neutral800)) : unusedColorPairs.removeFirst()
-                            lectureColorMap[lecture.id] = colorPair // 새 강의에 색상 맵핑
+                            colorPair = unusedColorPairs.isEmpty ?
+                                (UIColor.appColor(.neutral300), UIColor.appColor(.neutral800)) :
+                                unusedColorPairs.removeFirst()
+                            lectureColorMap[lecture.id] = colorPair
                         }
                         
                         let bodyColor = colorPair.0
                         let headerColor = colorPair.1
                         
-                        // LectureView 생성
                         let lectureView = LectureView(info: lecture, color: bodyColor)
-                        lectureView.separateView.backgroundColor = headerColor // separateView는 headerColor 사용
+                        lectureView.separateView.backgroundColor = headerColor
                         
-                        // 나머지 3개의 UI 요소에 bodyColor 적용
+                        // 기본 UI 설정
                         lectureView.backgroundColor = bodyColor
                         lectureView.lectureNameLabel.backgroundColor = bodyColor
                         lectureView.professorNameLabel.backgroundColor = bodyColor
                         
-                        // GestureRecognizer 추가
+                        // 탭 동작 추가
                         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleLectureTap(_:)))
                         lectureView.addGestureRecognizer(tapGesture)
                         lectureView.isUserInteractionEnabled = true
@@ -443,28 +439,28 @@ extension TimetableViewController {
                         lectureView.snp.makeConstraints { make in
                             make.height.equalTo(height * range.count - 2)
                             make.top.equalTo(containerView.snp.top).offset(height * (firstTime % 100) + 2)
+                            
                             let sectionWidth = containerView.frame.width / 5
                             let leadingOffset = sectionWidth * CGFloat(day)
                             let trailingOffset = sectionWidth * CGFloat(day + 1)
+                            
                             make.leading.equalTo(containerView.snp.leading).offset(leadingOffset + 1)
                             make.trailing.equalTo(containerView.snp.leading).offset(trailingOffset - 1)
                         }
+                        
                         if range.count == 1 {
                             lectureView.lectureNameLabel.snp.updateConstraints { make in
                                 make.height.equalTo(30)
                             }
                         }
                         
-                        
-                        // 새로 추가된 body 색상을 사용된 색상으로 추가
-                        usedColors.insert(bodyColor)
+                        usedColors.append(bodyColor)
                     }
                 }
             }
         }
     }
-    
-    
+ 
     @objc private func handleLectureTap(_ sender: UITapGestureRecognizer) {
         if let tappedView = sender.view as? LectureView {
             deleteLectureView.configure(lecture: tappedView.info)
