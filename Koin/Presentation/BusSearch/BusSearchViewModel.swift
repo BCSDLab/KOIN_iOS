91a9257a90f5869deec84ffe6fbc2f1d4b2ae0bc
Ju-Min-Kyung <alsrud7767@naver.com>
2024-12-16 13:30:04 +0900

    feat: 선택한 버스 장소가 모달에 그대로 유지되도록 수정

--- Koin/Presentation/BusSearch/BusSearchViewModel.swift
+++ Koin/Presentation/BusSearch/BusSearchViewModel.swift
@@ -9,21 +9,25 @@ import Combine
 
 final class BusSearchViewModel: ViewModelProtocol {
     enum Input {
-        
+       
     }
+    
     enum Output {
-        
+       
     }
     
     private let outputSubject = PassthroughSubject<Output, Never>()
     private var subscriptions: Set<AnyCancellable> = []
     
     func transform(with input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
-        
+        input.sink { [weak self] input in
+           
+        }.store(in: &subscriptions)
         return outputSubject.eraseToAnyPublisher()
     }
 }
 
 extension BusSearchViewModel {
-   
+    
+    
 }
