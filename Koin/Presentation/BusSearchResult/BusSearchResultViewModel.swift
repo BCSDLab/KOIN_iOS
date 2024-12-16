2a5c4e8ff802320a733f8ff0834eca436bc41132
Ju-Min-Kyung <alsrud7767@naver.com>
2024-12-16 13:21:16 +0900

    chore: 데이트피커의 데이터를 제공해주는 함수 유스케이스로 분리

--- Koin/Presentation/BusSearchResult/BusSearchResultViewModel.swift
+++ Koin/Presentation/BusSearchResult/BusSearchResultViewModel.swift
@@ -9,21 +9,34 @@ import Combine
 
 final class BusSearchResultViewModel: ViewModelProtocol {
     enum Input {
-        
+        case getDatePickerData
     }
     enum Output {
-        
+        case updateDatePickerData(([[String]], [String]))
     }
     
     private let outputSubject = PassthroughSubject<Output, Never>()
+    private let fetchDatePickerDataUseCase: FetchKoinPickerDataUseCase
     private var subscriptions: Set<AnyCancellable> = []
     
+    init(fetchDatePickerDataUseCase: FetchKoinPickerDataUseCase) {
+        self.fetchDatePickerDataUseCase = fetchDatePickerDataUseCase
+    }
+    
     func transform(with input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
-        
+        input.sink { [weak self] input in
+            switch input {
+            case .getDatePickerData:
+                self?.getDatePickerData()
+            }
+        }.store(in: &subscriptions)
         return outputSubject.eraseToAnyPublisher()
     }
 }
 
-extension BusSearchViewModel {
-   
+extension BusSearchResultViewModel {
+    private func getDatePickerData() {
+        let data = fetchDatePickerDataUseCase.execute()
+        outputSubject.send(.updateDatePickerData(data))
+    }
 }
