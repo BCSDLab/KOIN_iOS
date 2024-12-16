cbe370c9f7b7d6d5ffff9470be24b60358c4e888
Ju-Min-Kyung <alsrud7767@naver.com>
2024-12-16 12:56:21 +0900

    feat: 출발지 도착지 선택 바텀쉬트 적용

--- Koin/Presentation/BusSearch/BusSearchViewController.swift
+++ Koin/Presentation/BusSearch/BusSearchViewController.swift
@@ -7,6 +7,7 @@
 
 import Combine
 import DropDown
+import SnapKit
 import UIKit
 
 final class BusSearchViewController: CustomViewController {
@@ -16,8 +17,44 @@ final class BusSearchViewController: CustomViewController {
     private var subscriptions: Set<AnyCancellable> = []
     
     // MARK: - UI Components
-
-   
+    private let searchMainDescriptionLabel = UILabel().then {
+        $0.font = .appFont(.pretendardMedium, size: 16)
+        $0.textColor = .appColor(.neutral800)
+        $0.text = "목적지까지 가장 빠른 교통편을 알려드릴게요."
+    }
+    
+    private let searchSubDescriptionLabel = UILabel().then {
+        $0.font = .appFont(.pretendardRegular, size: 12)
+        $0.textColor = .appColor(.neutral600)
+        $0.text = "학기 중 시간표와 다를 수 있습니다."
+    }
+    
+    private let departGuideLabel = UILabel().then {
+        $0.text = "출발"
+    }
+    
+    private let arriveGuideLabel = UILabel().then {
+        $0.text = "도착"
+    }
+    
+    private let departAreaSelectedButton = UIButton()
+    
+    private let arrivedAreaSelectedButton = UIButton()
+    
+    private let swapAreaButton = UIButton().then {
+        $0.setImage(.appImage(asset: .swap), for: .normal)
+        $0.layer.cornerRadius = 16
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.appColor(.neutral300).cgColor
+    }
+    
+    private let busInfoSearchButton = UIButton().then {
+        $0.setAttributedTitle(NSAttributedString(string: "조회하기", attributes: [.font: UIFont.appFont(.pretendardMedium, size: 15)]), for: .normal)
+        $0.backgroundColor = .appColor(.neutral300)
+        $0.tintColor = .appColor(.neutral600)
+        $0.layer.cornerRadius = 4
+    }
+    
     // MARK: - Initialization
     
     init(viewModel: BusSearchViewModel) {
@@ -49,27 +86,101 @@ final class BusSearchViewController: CustomViewController {
 }
 
 extension BusSearchViewController {
-    
-   
-    
+    @objc private func tapBusAreaSelectedButtons(sender: UIButton) {
+        let busRouteType: Int
+        if sender == departAreaSelectedButton {
+            busRouteType = 0
+        }
+        else {
+           busRouteType = 1
+        }
+        let busAreaViewController = BusAreaSelectedViewController(busRouteType: busRouteType, busAreaLists: [.koreatech, .station, .terminal])
+        let bottomSheet = BottomSheetViewController(contentViewController: busAreaViewController, defaultHeight: 361, cornerRadius: 32, isPannedable: false)
+        self.present(bottomSheet, animated: true)
+    }
 }
 
 
 extension BusSearchViewController {
     
+    private func setUpLabels() {
+        [departGuideLabel, arriveGuideLabel].forEach {
+            $0.font = .appFont(.pretendardMedium, size: 16)
+            $0.textColor = .appColor(.primary500)
+            $0.textAlignment = .center
+        }
+    }
+    
+    private func setUpButtons() {
+        let buttonNames = ["출발지 선택", "목적지 선택"]
+        let buttons = [departAreaSelectedButton, arrivedAreaSelectedButton]
+        for (index, value) in buttons.enumerated() {
+            var configuration = UIButton.Configuration.plain()
+            configuration.attributedTitle = AttributedString(buttonNames[index], attributes: AttributeContainer([.font: UIFont.appFont(.pretendardRegular, size: 14), .foregroundColor: UIColor.appColor(.gray)]))
+            configuration.contentInsets = .init(top: 12, leading: 30, bottom: 12, trailing: 30)
+            value.configuration = configuration
+            value.backgroundColor = .appColor(.neutral100)
+            value.addTarget(self, action: #selector(tapBusAreaSelectedButtons), for: .touchUpInside)
+        }
+    }
+    
     private func setUpLayOuts() {
-        self.view.addSubview(navigationBarWrappedView)
+        [navigationBarWrappedView, searchMainDescriptionLabel, searchSubDescriptionLabel, departGuideLabel, arriveGuideLabel, departAreaSelectedButton, swapAreaButton, arrivedAreaSelectedButton, busInfoSearchButton].forEach {
+            view.addSubview($0)
+        }
     }
     
     private func setUpConstraints() {
         navigationBarWrappedView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.height.equalTo(85)
+            $0.height.equalTo(45)
+        }
+        searchMainDescriptionLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(24)
+            $0.top.equalTo(navigationBarWrappedView.snp.bottom).offset(16)
+        }
+        searchSubDescriptionLabel.snp.makeConstraints {
+            $0.leading.equalTo(searchMainDescriptionLabel)
+            $0.top.equalTo(searchMainDescriptionLabel.snp.bottom).offset(4)
+        }
+        departGuideLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(24)
+            $0.width.equalTo(127)
+            $0.top.equalTo(searchSubDescriptionLabel.snp.bottom).offset(46)
+        }
+        arriveGuideLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24)
+            $0.width.equalTo(127)
+            $0.top.equalTo(departGuideLabel)
+        }
+        departAreaSelectedButton.snp.makeConstraints {
+            $0.centerX.equalTo(departGuideLabel)
+            $0.top.equalTo(departGuideLabel.snp.bottom).offset(8)
+            $0.width.equalTo(143)
+            $0.height.equalTo(46)
+        }
+        arrivedAreaSelectedButton.snp.makeConstraints {
+            $0.centerX.equalTo(arriveGuideLabel)
+            $0.top.equalTo(departAreaSelectedButton)
+            $0.width.equalTo(143)
+            $0.height.equalTo(46)
+        }
+        swapAreaButton.snp.makeConstraints {
+            $0.width.height.equalTo(32)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(arrivedAreaSelectedButton)
+        }
+        busInfoSearchButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalToSuperview().inset(59)
+            $0.height.equalTo(48)
         }
     }
     
     private func configureView() {
+        setUpLabels()
+        setUpButtons()
         setUpLayOuts()
         setUpConstraints()
         self.view.backgroundColor = .systemBackground
---
+++ Koin/Presentation/BusSearch/SubViews/BusAreaSelected/BusAreaSelectdViewController.swift
@@ -0,0 +1,128 @@
+//
+//  BusAreaSelectedViewController.swift
+//  koin
+//
+//  Created by JOOMINKYUNG on 11/10/24.
+//
+
+import Combine
+import Then
+import UIKit
+
+final class BusAreaSelectedViewController: UIViewController {
+    //MARK: - Properties
+    let busAreaPublisher = PassthroughSubject<String, Never>()
+    private var busRouteType = 0 // 0이면 출발, 1이면 도착
+    private var busAreaLists: [BusPlace] = []
+    
+    //MARK: - UI Components
+    private let busRouteDescriptionlabel = UILabel().then {
+        $0.font = .appFont(.pretendardBold, size: 18)
+        $0.textColor = .appColor(.neutral800)
+    }
+    
+    private let busAreaCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 14
+        $0.collectionViewLayout = layout
+        $0.contentInset = .init(top: 16, left: 32, bottom: 16, right: 32)
+    }
+    
+    private let confirmButton = UIButton().then {
+        $0.backgroundColor = .appColor(.primary500)
+        $0.layer.cornerRadius = 4
+    }
+    
+    //MARK: - Initialization
+    init(busRouteType: Int, busAreaLists: [BusPlace]) {
+        self.busRouteType = busRouteType
+        self.busAreaLists = busAreaLists
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        busAreaCollectionView.register(BusAreaSelectedCollectionViewCell.self, forCellWithReuseIdentifier: BusAreaSelectedCollectionViewCell.identifier)
+        configureView()
+        setUpView()
+        busAreaCollectionView.delegate = self
+        busAreaCollectionView.dataSource = self
+        busAreaCollectionView.reloadData()
+    }
+}
+
+extension BusAreaSelectedViewController {
+    private func setUpView() {
+        let attributeContainer: [NSAttributedString.Key: Any] = [.font: UIFont.appFont(.pretendardMedium, size: 15), .foregroundColor: UIColor.appColor(.neutral0)]
+        if busRouteType == 0 { // 출발지 고르는 경우
+            busRouteDescriptionlabel.text = "어디서 출발하시나요?"
+            confirmButton.setAttributedTitle(NSAttributedString(string: "도착지 선택하기", attributes: attributeContainer), for: .normal)
+        }
+        else { // 도착지 고르는 경우
+            busRouteDescriptionlabel.text = "목적지가 어디인가요?"
+            confirmButton.setAttributedTitle(NSAttributedString(string: "확인하기", attributes: attributeContainer), for: .normal)
+        }
+    }
+}
+
+extension BusAreaSelectedViewController {
+    
+    private func setUpLayOuts() {
+        [busRouteDescriptionlabel, busAreaCollectionView, confirmButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setUpConstraints() {
+        busRouteDescriptionlabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(23)
+            $0.leading.equalToSuperview().offset(32)
+        }
+        busAreaCollectionView.snp.makeConstraints {
+            $0.top.equalTo(busRouteDescriptionlabel.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(80)
+        }
+        confirmButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(32)
+            $0.top.equalTo(busAreaCollectionView.snp.bottom).offset(83)
+            $0.height.equalTo(48)
+        }
+    }
+    
+    private func configureView() {
+        setUpLayOuts()
+        setUpConstraints()
+        self.view.backgroundColor = .systemBackground
+    }
+}
+
+extension BusAreaSelectedViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return busAreaLists.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BusAreaSelectedCollectionViewCell.identifier, for: indexPath) as? BusAreaSelectedCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(busPlace: busAreaLists[indexPath.row].koreanDescription, isSelected: false)
+        return cell
+    }
+}
+
+extension BusAreaSelectedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let label = UILabel()
+        label.text = busAreaLists[indexPath.row].koreanDescription
+        label.font = .appFont(.pretendardMedium, size: 15)
+        let size = label.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude, height: 48))
+        return CGSize(width: size.width + 32, height: 48)
+    }
+}
