f27446fcf27865e3def9b6ecad7fc1cd434ebd57
Ju-Min-Kyung <alsrud7767@naver.com>
2024-12-16 13:06:44 +0900

    add: 출발 시간과 버스의 종류로 필터링할 수 있는 버튼을 담은 테이블뷰 헤더 추가

---
+++ Koin/Presentation/BusSearchResult/SubViews/BusSearchResultTableView/BusSearchResultTableViewHeader.swift
@@ -0,0 +1,108 @@
+//
+//  BusSearchResultTableViewHeader.swift
+//  koin
+//
+//  Created by JOOMINKYUNG on 11/10/24.
+//
+
+import Combine
+import SnapKit
+import Then
+import UIKit
+
+final class BusSearchResultTableViewHeader: UITableViewHeaderFooterView {
+    // MARK: - Properties
+    var subscriptions = Set<AnyCancellable>()
+    
+    // MARK: - UIComponents
+    private let departTimeButton = UIView()
+    
+    private let departTimeLabel = UILabel().then {
+        $0.font = .appFont(.pretendardBold, size: 16)
+        $0.textColor = .appColor(.info700)
+        $0.textAlignment = .left
+    }
+    
+    private let busTypeFilterButton = UIButton().then {
+        $0.layer.cornerRadius = 12
+        $0.backgroundColor = .appColor(.neutral50)
+    }
+   
+    // MARK: - Initialization
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configureView()
+    }
+    
+    override func prepareForReuse() {
+        subscriptions.removeAll()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+    
+    func configure(departTime: String, busType: String) {
+        departTimeLabel.text = departTime
+        setUpBusTypeFilterButton(busType: busType)
+    }
+}
+
+extension BusSearchResultTableViewHeader {
+    private func setUpDepartTimeButton() {
+        let imageView = UIImageView(image: .appImage(asset: .arrowDown))
+        let departLabel = UILabel()
+        departLabel.text = "출발"
+        departLabel.font = .appFont(.pretendardBold, size: 16)
+        departLabel.textColor = .black
+        [imageView, departLabel, departTimeLabel].forEach {
+            departTimeButton.addSubview($0)
+        }
+        departTimeLabel.snp.makeConstraints {
+            $0.leading.centerX.equalToSuperview()
+        }
+        imageView.snp.makeConstraints {
+            $0.centerY.trailing.equalToSuperview()
+            $0.width.height.equalTo(24)
+        }
+        departLabel.snp.makeConstraints {
+            $0.leading.equalTo(departTimeLabel.snp.trailing)
+            $0.centerX.equalTo(departTimeLabel)
+        }
+    }
+    
+    private func setUpBusTypeFilterButton(busType: String) {
+        var configuration = UIButton.Configuration.plain()
+        configuration.attributedTitle = AttributedString(busType, attributes: AttributeContainer([.font: UIFont.appFont(.pretendardMedium, size: 14), .foregroundColor: UIColor.appColor(.neutral800)]))
+        configuration.image = .appImage(asset: .arrowDown)
+        configuration.imagePadding = 4
+        configuration.contentInsets = .init(top: 9, leading: 16, bottom: 9, trailing: 16)
+        busTypeFilterButton.configuration = configuration
+    }
+    
+    private func setUpLayouts() {
+        [busTypeFilterButton, departTimeButton].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setUpConstraints() {
+        departTimeButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(24)
+            $0.top.equalToSuperview().offset(23)
+            $0.height.equalTo(26)
+        }
+        busTypeFilterButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24)
+            $0.centerX.equalTo(departTimeLabel)
+        }
+    }
+    
+    private func configureView() {
+        setUpLayouts()
+        setUpConstraints()
+        
+    }
+}
