6befc6d1a9d071c3a13e7a027241cf6cf9ed25df
Ju-Min-Kyung <alsrud7767@naver.com>
2024-12-19 14:47:50 +0900

    fix: 대성버스 오전 오후 나눔

--- Koin/Data/DTOs/Decodable/Bus/ExpressBusTimeTableDTO.swift
+++ Koin/Data/DTOs/Decodable/Bus/ExpressBusTimeTableDTO.swift
@@ -32,13 +32,50 @@ extension ExpressTimetableDTO {
             let updatedComponents = updatedAt.components(separatedBy: " ")
             updatedAtText = updatedComponents.first ?? ""
         }
-        if let expressTimeTable = expressTimeTable {
-            for timetable in expressTimeTable {
-                let arrivalInfo: BusArrivalInfo = BusArrivalInfo(leftNode: timetable.departure, rightNode: timetable.arrival)
+        if let departInfos = expressTimeTable {
+            var numberOfMorning: Int = -1
+            for idx in 0..<departInfos.count {
+                if !isMorning(timeString: departInfos[idx].departure) {
+                    numberOfMorning = idx
+                    break
+                }
+            }
+            
+            let numberOfAfternoon: Int = departInfos.count - numberOfMorning
+            let lastMorningAndAfternoonIdx: Int = numberOfAfternoon > numberOfMorning ? numberOfMorning : numberOfAfternoon
+            for idx in 0..<lastMorningAndAfternoonIdx {
+                let arrivalInfo = BusArrivalInfo(leftNode: departInfos[idx].departure, rightNode: departInfos[idx + lastMorningAndAfternoonIdx].departure)
                 arrivalInfos.append(arrivalInfo)
+            }
+            
+            if numberOfAfternoon > numberOfMorning {
+                for idx in lastMorningAndAfternoonIdx * 2..<departInfos.count {
+                    let arrivalInfo = BusArrivalInfo(leftNode: nil, rightNode: departInfos[idx].departure)
+                    arrivalInfos.append(arrivalInfo)
+                }
+            }
+            else if numberOfAfternoon < numberOfMorning {
+                for idx in lastMorningAndAfternoonIdx..<departInfos.count {
+                    let arrivalInfo = BusArrivalInfo(leftNode: departInfos[idx].departure, rightNode: nil)
+                    arrivalInfos.append(arrivalInfo)
+                }
             }
         }
         let timetableinfo = BusTimetableInfo(arrivalInfos: arrivalInfos, updatedAt: updatedAtText)
         return timetableinfo
+    }
+    
+    private func isMorning(timeString: String) -> Bool {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        guard let date = dateFormatter.date(from: timeString) else {
+            return false
+        }
+        
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        
+        return hour >= 0 && hour < 12
     }
 }
