7a9c88b347d98b4553979585a9f3d070f13eac1f
KimNahun <rlaskgns0212@naver.com>
2025-01-15 19:42:01 +0900

    add: 분실물 관련 DTO 모델 추가

---
+++ Koin/Data/DTOs/Decodable/NoticeList/PostLostArticleResponse.swift
@@ -0,0 +1,23 @@
+//
+//  PostLostArticleResponse.swift
+//  koin
+//
+//  Created by 김나훈 on 1/15/25.
+//
+
+// MARK: - Article
+
+struct PostLostArticleResponse: Codable {
+    let category, location, foundDate: String
+    let content: String?
+    let images: [String]?
+    let registeredAt, updatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case category, location
+        case foundDate = "found_date"
+        case content, images
+        case registeredAt = "registered_at"
+        case updatedAt = "updated_at"
+    }
+}
