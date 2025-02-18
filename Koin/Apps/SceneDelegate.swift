a2e4dc0b917307c225d5e3dc4c46624608633c3c
KimNahun <rlaskgns0212@naver.com>
2025-02-18 16:58:51 +0900

    feat: 쪽지 알림 switch 구현

--- Koin/Apps/SceneDelegate.swift
+++ Koin/Apps/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
     
-        let navigationController = CustomNavigationController(rootViewController: ChatListTableViewController(viewModel: ChatListTableViewModel()))
+        let navigationController = CustomNavigationController(rootViewController: makeHomeViewController())
         window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
