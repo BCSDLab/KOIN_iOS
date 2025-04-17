45f3e847bff4744444c496f86e3d2c74bdc8c845
eunji <oieunxe@gmail.com>
2025-04-17 11:14:12 +0900

    feat: 회원가입 버튼 클릭 시 이동 뷰 변경

--- Koin/Presentation/Login/LoginViewController.swift
+++ Koin/Presentation/Login/LoginViewController.swift
@@ -155,7 +155,8 @@ extension LoginViewController {
         let timetableRepositoy = DefaultTimetableRepository(service: DefaultTimetableService())
         let userRepository = DefaultUserRepository(service: DefaultUserService())
         let logRepoository = GA4AnalyticsRepository(service: GA4AnalyticsService())
-        let registerViewController = RegisterViewController(viewModel: RegisterViewModel(fetchDeptListUseCase: DefaultFetchDeptListUseCase(timetableRepository: timetableRepositoy), registerUseCase: DefaultRegisterUseCase(userRepository: userRepository), checkDuplicatedNicknameUseCase: DefaultCheckDuplicatedNicknameUseCase(userRepository: userRepository), logAnalyticsEventUseCase: DefaultLogAnalyticsEventUseCase(repository: logRepoository)))
+        // let registerViewController = RegisterViewController(viewModel: RegisterViewModel(fetchDeptListUseCase: DefaultFetchDeptListUseCase(timetableRepository: timetableRepositoy), registerUseCase: DefaultRegisterUseCase(userRepository: userRepository), checkDuplicatedNicknameUseCase: DefaultCheckDuplicatedNicknameUseCase(userRepository: userRepository), logAnalyticsEventUseCase: DefaultLogAnalyticsEventUseCase(repository: logRepoository)))
+        let registerViewController = RegisterFormViewController(viewModel: RegisterFormViewModel())
         registerViewController.title = "회원가입"
         navigationController?.pushViewController(registerViewController, animated: true)
         
