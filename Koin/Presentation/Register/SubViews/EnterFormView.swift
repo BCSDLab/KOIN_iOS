645c85bf926c0e6918696ab0b6362afedce1fc2d
eunji <oieunxe@gmail.com>
2025-04-22 16:51:23 +0900

    feat: 정보 입력 아이디 텍스트 필드 추가

--- Koin/Presentation/Register/SubViews/EnterFormView.swift
+++ Koin/Presentation/Register/SubViews/EnterFormView.swift
@@ -6,17 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
 final class EnterFormView: UIView {
     
     // MARK: - Properties
     private let viewModel: RegisterFormViewModel
     
-    // MARK: - UI Components
+    // MARK: - UI Components 16
+    private let idLabel = UILabel().then {
+        $0.font = UIFont.appFont(.pretendardMedium, size: 18)
+        $0.textColor = .black
+        $0.text = "아이디 (전화번호)"
+    }
     
+    private let seperateView1 = UIView().then {
+        $0.backgroundColor = .appColor(.neutral300)
+    }
+    
+    private let idTextField = UITextField().then {
+        $0.attributedPlaceholder = NSAttributedString(string: "아이디", attributes: [.foregroundColor: UIColor.appColor(.neutral400), .font: UIFont.appFont(.pretendardRegular, size: 14)])
+        $0.autocapitalizationType = .none
+        $0.font = UIFont.appFont(.pretendardRegular, size: 14)
+    }
     
     // MARK: Init
-    
      init(viewModel: RegisterFormViewModel) {
         self.viewModel = viewModel
         super.init(frame: .zero)
@@ -37,18 +51,35 @@ extension EnterFormView {
 
 extension EnterFormView {
     private func setUpLayOuts() {
-        [].forEach {
+        [idLabel, idTextField, seperateView1].forEach {
             self.addSubview($0)
         }
     }
     
     private func setUpConstraints() {
+        idLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(16)
+            $0.leading.equalToSuperview().offset(8)
+            $0.height.equalTo(29)
+        }
         
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(idLabel.snp.bottom).offset(12)
+            $0.leading.equalTo(idLabel.snp.leading).offset(4)
+            $0.trailing.equalToSuperview().offset(-8)
+            $0.height.equalTo(40)
+        }
+        
+        seperateView1.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom)
+            $0.leading.equalTo(idLabel.snp.leading)
+            $0.trailing.equalTo(idTextField.snp.trailing)
+            $0.height.equalTo(1)
+        }
     }
     
     private func configureView() {
         setUpLayOuts()
         setUpConstraints()
-        self.backgroundColor = .brown
     }
 }
