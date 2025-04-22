2c496d9717fa9cb9962f41eb23d854aa4f9f6bdf
eunji <oieunxe@gmail.com>
2025-04-22 17:27:19 +0900

    feat: 비밀번호 입력 텍스트 필드 추가

--- Koin/Presentation/Register/SubViews/EnterFormView.swift
+++ Koin/Presentation/Register/SubViews/EnterFormView.swift
@@ -30,6 +30,44 @@ final class EnterFormView: UIView {
         $0.font = UIFont.appFont(.pretendardRegular, size: 14)
     }
     
+    private let passwordLabel = UILabel().then {
+        $0.font = UIFont.appFont(.pretendardMedium, size: 18)
+        $0.textColor = .black
+        $0.text = "사용하실 비밀번호를 입력해 주세요."
+    }
+    
+    private let passwordTextField1 = UITextField().then {
+        $0.attributedPlaceholder = NSAttributedString(string: "특수문자 포함 영어와 숫자 6~18자리로 입력해주세요.", attributes: [.foregroundColor: UIColor.appColor(.neutral400), .font: UIFont.appFont(.pretendardRegular, size: 13)])
+        $0.font = UIFont.appFont(.pretendardRegular, size: 14)
+        $0.autocapitalizationType = .none
+        $0.isSecureTextEntry = true
+    }
+    
+    private let changeSecureButton1 = UIButton().then { button in
+        button.setImage(UIImage.appImage(asset: .visibility), for: .normal)
+        button.addTarget(self, action: #selector(changeSecureButtonTapped1), for: .touchUpInside)
+    }
+    
+    private let seperateView2 = UIView().then {
+        $0.backgroundColor = .appColor(.neutral300)
+    }
+    
+    private let passwordTextField2 = UITextField().then {
+        $0.attributedPlaceholder = NSAttributedString(string: "비밀번호를 다시 입력해 주세요.", attributes: [.foregroundColor: UIColor.appColor(.neutral400), .font: UIFont.appFont(.pretendardRegular, size: 13)])
+        $0.font = UIFont.appFont(.pretendardRegular, size: 14)
+        $0.autocapitalizationType = .none
+        $0.isSecureTextEntry = true
+    }
+    
+    private let changeSecureButton2 = UIButton().then { button in
+        button.setImage(UIImage.appImage(asset: .visibility), for: .normal)
+        button.addTarget(self, action: #selector(changeSecureButtonTapped2), for: .touchUpInside)
+    }
+    
+    private let seperateView3 = UIView().then {
+        $0.backgroundColor = .appColor(.neutral300)
+    }
+    
     // MARK: Init
      init(viewModel: RegisterFormViewModel) {
         self.viewModel = viewModel
@@ -41,6 +79,16 @@ final class EnterFormView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    @objc private func changeSecureButtonTapped1() {
+        passwordTextField1.isSecureTextEntry.toggle()
+        changeSecureButton1.setImage(passwordTextField1.isSecureTextEntry ? UIImage.appImage(asset: .visibility) : UIImage.appImage(asset: .visibilityNon), for: .normal)
+    }
+    
+    @objc private func changeSecureButtonTapped2() {
+        passwordTextField2.isSecureTextEntry.toggle()
+        changeSecureButton2.setImage(passwordTextField2.isSecureTextEntry ? UIImage.appImage(asset: .visibility) : UIImage.appImage(asset: .visibilityNon), for: .normal)
+    }
 }
 
 extension EnterFormView {
@@ -51,7 +99,7 @@ extension EnterFormView {
 
 extension EnterFormView {
     private func setUpLayOuts() {
-        [idLabel, idTextField, seperateView1].forEach {
+        [idLabel, idTextField, seperateView1, passwordLabel, passwordTextField1, changeSecureButton1, seperateView2, passwordTextField2, changeSecureButton2, seperateView3].forEach {
             self.addSubview($0)
         }
     }
@@ -76,6 +124,53 @@ extension EnterFormView {
             $0.trailing.equalTo(idTextField.snp.trailing)
             $0.height.equalTo(1)
         }
+        
+        passwordLabel.snp.makeConstraints {
+            $0.top.equalTo(seperateView1.snp.bottom).offset(32)
+            $0.leading.equalToSuperview().offset(8)
+            $0.height.equalTo(29)
+        }
+        
+        passwordTextField1.snp.makeConstraints {
+            $0.top.equalTo(passwordLabel.snp.bottom).offset(12)
+            $0.leading.equalTo(passwordLabel.snp.leading).offset(4)
+            $0.trailing.equalToSuperview().offset(-40)
+            $0.height.equalTo(40)
+        }
+        
+        changeSecureButton1.snp.makeConstraints {
+            $0.centerY.equalTo(passwordTextField1.snp.centerY)
+            $0.trailing.equalToSuperview().offset(-8)
+            $0.width.height.equalTo(20)
+        }
+        
+        seperateView2.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField1.snp.bottom)
+            $0.leading.equalTo(passwordLabel.snp.leading)
+            $0.trailing.equalTo(idTextField.snp.trailing)
+            $0.height.equalTo(1)
+        }
+        
+        passwordTextField2.snp.makeConstraints {
+            $0.top.equalTo(seperateView2.snp.bottom).offset(12)
+            $0.leading.equalTo(passwordLabel.snp.leading).offset(4)
+            $0.trailing.equalToSuperview().offset(-40)
+            $0.height.equalTo(40)
+        }
+        
+        changeSecureButton2.snp.makeConstraints {
+            $0.centerY.equalTo(passwordTextField2.snp.centerY)
+            $0.trailing.equalToSuperview().offset(-8)
+            $0.width.height.equalTo(20)
+        }
+        
+        seperateView3.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField2.snp.bottom)
+            $0.leading.equalTo(passwordLabel.snp.leading)
+            $0.trailing.equalTo(idTextField.snp.trailing)
+            $0.height.equalTo(1)
+        }
+        
     }
     
     private func configureView() {
