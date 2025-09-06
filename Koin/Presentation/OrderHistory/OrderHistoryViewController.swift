ecd2a679dd1ab6ba2d7ed4e9e628f94753623caa
xp65241 <xp65241@naver.com>
2025-09-06 13:24:50 +0900

    feat: orderHistorySegment 구현

--- Koin/Presentation/OrderHistory/OrderHistoryViewController.swift
+++ Koin/Presentation/OrderHistory/OrderHistoryViewController.swift
@@ -6,11 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 final class OrderHistoryViewController: UIViewController {
     
     // MARK: - UI Components
-
+    let orderHistorySegment: UISegmentedControl = {
+        let segment = UISegmentedControl()
+        segment.insertSegment(withTitle: "지난 주문", at: 0, animated: true)
+        segment.insertSegment(withTitle: "준비 중", at: 1, animated: true)
+        
+        segment.setTitleTextAttributes([
+            .foregroundColor: UIColor.appColor(.neutral500),
+            .font: UIFont.appFont(.pretendardBold, size: 16)
+        ], for: .normal)
+        
+        segment.setTitleTextAttributes([
+            .foregroundColor: UIColor.appColor(.new500),
+            .font: UIFont.appFont(.pretendardBold, size: 16),
+        ], for: .selected)
+        
+        segment.selectedSegmentTintColor = .clear
+        segment.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
+        segment.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        return segment
+    }()
+    
+    let orderHistorySeperateView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .appColor(.neutral400)
+        return view
+    }()
+    
+    let orderHistoryUnderLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .appColor(.new500)
+        return view
+    }()
+    
     
     // MARK: - Initialization
     
@@ -29,9 +62,34 @@ final class OrderHistoryViewController: UIViewController {
 extension OrderHistoryViewController {
     
     private func setUpLayOuts() {
+        [orderHistorySegment, orderHistorySeperateView, orderHistoryUnderLineView].forEach {
+            view.addSubview($0)
+        }
+        
+        orderHistorySegment.addTarget(self, action: #selector(changeSegmentLine(_:)), for: .valueChanged)
+        
     }
     
     private func setUpConstraints() {
+        orderHistorySegment.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        
+        orderHistorySeperateView.snp.makeConstraints {
+            $0.bottom.equalTo(orderHistorySegment.snp.bottom)
+            $0.width.equalTo(UIScreen.main.bounds.width)
+            $0.height.equalTo(1)
+        }
+        
+        orderHistoryUnderLineView.snp.makeConstraints {
+            $0.bottom.equalTo(orderHistorySegment.snp.bottom)
+            $0.width.equalTo((UIScreen.main.bounds.width/2) - 15)
+            $0.height.equalTo(2)
+            $0.leading.equalTo(orderHistorySegment.snp.leading).offset(7.5)
+        }
+        
 
     }
     
@@ -40,5 +98,23 @@ extension OrderHistoryViewController {
         setUpConstraints()
         self.view.backgroundColor = UIColor.appColor(.newBackground)
     }
+    
+    
 }
 
+
+
+extension OrderHistoryViewController {
+    @objc private func changeSegmentLine(_ segment: UISegmentedControl){
+        let segmentCount = CGFloat(segment.numberOfSegments)
+        let leadingDistance: CGFloat = CGFloat(segment.selectedSegmentIndex) * (UIScreen.main.bounds.width / segmentCount) + 7.5
+        
+        UIView.animate(withDuration:0.2, animations: {
+            self.orderHistoryUnderLineView.snp.updateConstraints {
+                $0.leading.equalTo(self.orderHistorySegment.snp.leading).offset(leadingDistance)
+            }
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+}
