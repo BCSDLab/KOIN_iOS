a0b1f2e7c69b62877ea1376228fcbcb1e5ef50ad
eunji <oieunxe@gmail.com>
2025-09-06 22:36:45 +0900

    design: 재설치 버튼 클릭 시 팝업 UI 구현

--- Koin/Presentation/ForceUpdate/UpdateModelViewController.swift
+++ Koin/Presentation/ForceUpdate/UpdateModelViewController.swift
@@ -7,19 +7,22 @@
 
 import Combine
 import UIKit
+import SnapKit
 
 final class UpdateModelViewController: UIViewController {
     
+    // MARK: - Properties
     let openStoreButtonPublisher = PassthroughSubject<Void, Never>()
     let cancelButtonPublisher = PassthroughSubject<Void, Never>()
     
+    // MARK: - UI Components
     private let messageLabel = UILabel().then {
         $0.font = UIFont.appFont(.pretendardMedium, size: 18)
-        $0.text = "이미 업데이트를 하셨나요?"
+        $0.text = "이미 업데이트 하셨나요?"
     }
     
     private let subMessageLabel = UILabel().then {
-        $0.font = UIFont.appFont(.pretendardRegular, size: 14)
+        $0.font = UIFont.appFont(.pretendardRegular, size: 13)
         $0.textColor = UIColor.appColor(.neutral600)
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 6
@@ -33,39 +36,45 @@ final class UpdateModelViewController: UIViewController {
     
     private let closeButton = UIButton().then {
         $0.backgroundColor = UIColor.appColor(.neutral0)
-        $0.layer.borderColor = UIColor.appColor(.neutral500).cgColor
+        $0.layer.borderColor = UIColor.appColor(.neutral400).cgColor
         $0.layer.borderWidth = 1.0
-        $0.setTitle("확인", for: .normal)
+        $0.setTitle("닫기", for: .normal)
         $0.setTitleColor(UIColor.appColor(.neutral600), for: .normal)
-        $0.titleLabel?.font = UIFont.appFont(.pretendardBold, size: 15)
-        $0.layer.cornerRadius = 4
+        $0.titleLabel?.font = UIFont.appFont(.pretendardMedium, size: 15)
+        $0.layer.cornerRadius = 6
         $0.layer.masksToBounds = true
     }
     
     private let openStoreButton = UIButton().then {
-        $0.backgroundColor = UIColor.appColor(.primary500)
-        $0.setTitle("스토어로 가기", for: .normal)
+        $0.backgroundColor = UIColor.appColor(.new500)
+        $0.setTitle("스토어 가기", for: .normal)
         $0.setTitleColor(UIColor.appColor(.neutral0), for: .normal)
         $0.titleLabel?.font = UIFont.appFont(.pretendardMedium, size: 15)
+        $0.layer.cornerRadius = 6
+        $0.layer.masksToBounds = true
+    }
+    
+    private let containerView = UIView().then {
+        $0.backgroundColor = .white
         $0.layer.cornerRadius = 4
         $0.layer.masksToBounds = true
     }
     
-    private let containerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 4
-        view.layer.masksToBounds = true
-        return view
-    }()
-    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        setAddTarget()
+    }
+    
+    private func setAddTarget() {
         openStoreButton.addTarget(self, action: #selector(openStoreButtonTapped), for: .touchUpInside)
         closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
     }
-    
+}
+
+// MARK: - @objc
+extension UpdateModelViewController {
     @objc private func closeButtonTapped() {
         dismiss(animated: true, completion: nil)
         cancelButtonPublisher.send()
@@ -77,43 +86,48 @@ final class UpdateModelViewController: UIViewController {
     }
 }
 
+// MARK: - UI Function
 extension UpdateModelViewController {
-    
     private func setUpLayOuts() {
         [containerView].forEach {
             view.addSubview($0)
         }
+        
         [messageLabel, subMessageLabel, closeButton, openStoreButton].forEach {
             containerView.addSubview($0)
         }
     }
     
     private func setUpConstraints() {
-        containerView.snp.makeConstraints { make in
-            make.centerX.equalTo(view.snp.centerX)
-            make.centerY.equalTo(view.snp.centerY)
-            make.width.equalTo(301)
-            make.height.equalTo(210)
+        containerView.snp.makeConstraints {
+            $0.centerX.equalTo(view.snp.centerX)
+            $0.centerY.equalTo(view.snp.centerY)
+            $0.width.equalTo(301)
+            $0.height.equalTo(210)
         }
-        messageLabel.snp.makeConstraints { make in
-            make.top.equalTo(containerView.snp.top).offset(24)
-            make.centerX.equalTo(containerView.snp.centerX)
+        
+        messageLabel.snp.makeConstraints {
+            $0.top.equalTo(containerView.snp.top).offset(24)
+            $0.centerX.equalTo(containerView.snp.centerX)
         }
-        subMessageLabel.snp.makeConstraints { make in
-            make.top.equalTo(messageLabel.snp.bottom).offset(16)
-            make.centerX.equalTo(containerView.snp.centerX)
+        
+        subMessageLabel.snp.makeConstraints {
+            $0.top.equalTo(messageLabel.snp.bottom).offset(16)
+            $0.centerX.equalTo(containerView.snp.centerX)
         }
-        closeButton.snp.makeConstraints { make in
-            make.top.equalTo(subMessageLabel.snp.bottom).offset(24)
-            make.trailing.equalTo(containerView.snp.centerX).offset(-2)
-            make.width.equalTo(114.5)
-            make.height.equalTo(48)
+        
+        closeButton.snp.makeConstraints {
+            $0.top.equalTo(subMessageLabel.snp.bottom).offset(24)
+            $0.trailing.equalTo(containerView.snp.centerX).offset(-4)
+            $0.width.equalTo(114.5)
+            $0.height.equalTo(48)
         }
-        openStoreButton.snp.makeConstraints { make in
-            make.top.equalTo(subMessageLabel.snp.bottom).offset(24)
-            make.leading.equalTo(containerView.snp.centerX).offset(2)
-            make.width.equalTo(114.5)
-            make.height.equalTo(48)
+        
+        openStoreButton.snp.makeConstraints {
+            $0.top.equalTo(subMessageLabel.snp.bottom).offset(24)
+            $0.leading.equalTo(containerView.snp.centerX).offset(4)
+            $0.width.equalTo(114.5)
+            $0.height.equalTo(48)
         }
     }
     
