c371c84926dd55143baffdb4e431c4fef82f3053
eunji <oieunxe@gmail.com>
2025-09-19 22:07:07 +0900

    feat: 셔틀 버스 시간표 Model 구현

--- Koin/Data/DTOs/Decodable/Bus/ShuttleBusTimetableDto.swift
+++ Koin/Data/DTOs/Decodable/Bus/ShuttleBusTimetableDto.swift
@@ -1,5 +1,5 @@
 //
-//  ShuttleBusTimetableDTO.swift
+//  ShuttleBusTimetableDto.swift
 //  koin
 //
 //  Created by JOOMINKYUNG on 12/8/24.
@@ -7,13 +7,12 @@
 
 import Foundation
 
-// MARK: - ShuttleBusTimetableInfo
-struct ShuttleBusTimetableDTO: Decodable {
+struct ShuttleBusTimetableDto: Decodable {
     let id, region, routeName: String
     let routeType: ShuttleRouteType
     let subName: String?
-    let nodeInfo: [NodeInfo]
-    let routeInfo: [RouteInfo]
+    let nodeInfo: [NodeInfoDto]
+    let routeInfo: [RouteInfoDto]
 
     enum CodingKeys: String, CodingKey {
         case id, region
@@ -25,14 +24,12 @@ struct ShuttleBusTimetableDTO: Decodable {
     }
 }
 
-// MARK: - NodeInfo
-struct NodeInfo: Decodable {
+struct NodeInfoDto: Decodable {
     let name: String
     let detail: String?
 }
 
-// MARK: - RouteInfo
-struct RouteInfo: Decodable {
+struct RouteInfoDto: Decodable {
     let name: String
     let detail: String?
     let arrivalTime: [String?]
@@ -43,37 +40,43 @@ struct RouteInfo: Decodable {
     }
 }
 
-extension ShuttleBusTimetableDTO {
-    func toDomain() -> ShuttleBusTimetableDTO {
+extension ShuttleBusTimetableDto {
+    func toDomain() -> ShuttleBusTimetable {
         let hasGoSchool = routeInfo.contains { $0.name == "등교" }
         let hasDropOffSchool = routeInfo.contains { $0.name == "하교" }
 
-        let updatedRouteInfo = routeInfo.map { route in
-            if route.name == "하교" && hasGoSchool && hasDropOffSchool {
-                return route.toDomain()
-            } else {
-                return route
-            }
+        let updatedRouteInfo = routeInfo.map { dto in
+            dto.toDomain(hasGoSchool: hasGoSchool, hasDropOffSchool: hasDropOffSchool)
         }
 
-        return ShuttleBusTimetableDTO(
+        return ShuttleBusTimetable(
             id: id,
             region: region,
             routeName: routeName,
             routeType: routeType,
             subName: subName,
-            nodeInfo: nodeInfo,
+            nodeInfo: nodeInfo.map { $0.toDomain() },
             routeInfo: updatedRouteInfo
         )
     }
 }
 
-extension RouteInfo {
-    func toDomain() -> RouteInfo {
+extension NodeInfoDto {
+    func toDomain() -> NodeInfo {
+        NodeInfo(name: name, detail: detail)
+    }
+}
+
+extension RouteInfoDto {
+    func toDomain(hasGoSchool: Bool, hasDropOffSchool: Bool) -> RouteInfo {
+        let reversedArrival = (name == "하교" && hasGoSchool && hasDropOffSchool)
+            ? arrivalTime.reversed()
+            : arrivalTime
+
         return RouteInfo(
             name: name,
             detail: detail,
-            arrivalTime: name == "하교" ? arrivalTime.reversed() : arrivalTime
+            arrivalTime: Array(reversedArrival)
         )
     }
 }
---
+++ Koin/Domain/Model/Bus/ShuttleBusTimetable.swift
@@ -0,0 +1,8 @@
+//
+//  ShuttleBusTimetable.swift
+//  koin
+//
+//  Created by 이은지 on 9/19/25.
+//
+
+import Foundation
