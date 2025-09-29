07dc15335d5b13910f3df184b5d2e6773836a45a
GI JEONG HONG <hgjwilly@gmail.com>
2025-09-29 14:03:18 +0900

    add: Cart Model 추가

---
+++ Koin/Presentation/OrderCart/Subviews/OrderCartTableView/OrderCartListCell.swift
@@ -0,0 +1,13 @@
+//
+//  OrderCartCartListCell.swift
+//  koin
+//
+//  Created by 홍기정 on 9/29/25.
+//
+
+import UIKit
+
+final class OrderCartCartListCell: UITableViewCell {
+    
+    
+}
---
+++ Koin/Presentation/OrderCart/Subviews/OrderCartTableView/OrderCartShopTitleHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  TableHeaderView.swift
+//  koin
+//
+//  Created by 홍기정 on 9/29/25.
+//
+
+import Foundation
