3e1314999a4f466ebce8209dda29870d6fb110d9
oeunji <oieunxe@gmail.com>
2025-10-22 22:51:42 +0900

    refactor: 헤더뷰 코드 가독성 개선 및 컬러 수정

--- Koin/Presentation/ShopData/SubViews/ReviewListCollectionView/ReviewListHeaderView.swift
+++ Koin/Presentation/ShopData/SubViews/ReviewListCollectionView/ReviewListHeaderView.swift
@@ -6,86 +6,95 @@
 //
 
 import Combine
-import DropDown
-import Then
 import UIKit
+import DropDown
+import SnapKit
 
 final class ReviewListHeaderView: UICollectionReusableView {
     
+    // MARK: - Constants
+    
     static let identifier = "ReviewListHeaderView"
+    
+    // MARK: - Publishers
+    
     let sortTypeButtonPublisher = PassthroughSubject<ReviewSortType, Never>()
     let myReviewButtonPublisher = PassthroughSubject<Bool, Never>()
     
+    // MARK: - UI Components
+    
     private let separateView = UIView().then {
-        $0.backgroundColor = UIColor.appColor(.neutral200)
+        $0.backgroundColor = UIColor.appColor(.neutral300)
     }
     
-    private let sortTypeButton = UIButton().then { _ in
-    }
+    private let sortTypeButton = UIButton()
     
-    private let myReviewButton = UIButton().then { _ in
-    }
+    private let myReviewButton = UIButton()
     
     private let dropDown = DropDown()
     
+    // MARK: - Initialize
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupViews()
-        setUpSortTypeButtonText(type: .latest)
-        setUpMyReviewButtonText()
-        setUpDropDown()
-        sortTypeButton.addTarget(self, action: #selector(sortTypeButtonTapped), for: .touchUpInside)
-        myReviewButton.addTarget(self, action: #selector(myReviewButtonTapped), for: .touchUpInside)
+        configureView()
+        configureButtons()
+        setupDropDown()
+        setAddTarget()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+        
     func updateHeader(fetchStandard: ReviewSortType, isMine: Bool) {
-            setUpSortTypeButtonText(type: fetchStandard)
+        configureSortTypeButton(type: fetchStandard)
         myReviewButton.isSelected = isMine
-                setUpMyReviewButtonText()
-        }
+        configureMyReviewButton()
+    }
+}
 
+extension ReviewListHeaderView {
     
-    private func setUpDropDown() {
+    private func configureButtons() {
+        configureSortTypeButton(type: .latest)
+        configureMyReviewButton()
+    }
+    
+    private func setAddTarget() {
+        sortTypeButton.addTarget(self, action: #selector(sortTypeButtonTapped), for: .touchUpInside)
+        myReviewButton.addTarget(self, action: #selector(myReviewButtonTapped), for: .touchUpInside)
+    }
+    
+    private func setupDropDown() {
         dropDown.anchorView = sortTypeButton
         dropDown.bottomOffset = CGPoint(x: 0, y: sortTypeButton.bounds.height + 22)
         dropDown.direction = .bottom
         dropDown.dataSource = ReviewSortType.allCases.map { $0.koreanDescription }
-        dropDown.selectionAction = { [weak self] index, item in
-            guard let self = self else { return }
+        
+        dropDown.selectionAction = { [weak self] index, _ in
+            guard let self else { return }
             let selectedSortType = ReviewSortType.allCases[index]
-            self.setUpSortTypeButtonText(type: selectedSortType)
-            sortTypeButtonPublisher.send(selectedSortType)
+            self.configureSortTypeButton(type: selectedSortType)
+            self.sortTypeButtonPublisher.send(selectedSortType)
         }
+        
         dropDown.cancelAction = { [weak self] in
             self?.updateSortTypeButtonImage(for: .chevronDown)
         }
     }
-    @objc private func sortTypeButtonTapped() {
-        var configuration = sortTypeButton.configuration
-        let imageSize = CGSize(width: 15, height: 12)
-        let image = UIImage(systemName: SFSymbols.chevronUp.rawValue)
-        let resizedImage = image?.withConfiguration(
-            UIImage.SymbolConfiguration(pointSize: imageSize.width, weight: .regular)
-        )
-        configuration?.image = resizedImage
-        sortTypeButton.configuration = configuration
-        dropDown.show()
-    }
+}
+
+// MARK: - Button Configuration
+extension ReviewListHeaderView {
     
-    @objc private func myReviewButtonTapped() {
-        myReviewButton.isSelected.toggle()
-        myReviewButtonPublisher.send(myReviewButton.isSelected)
-    }
-    
-    private func setUpSortTypeButtonText(type: ReviewSortType) {
+    private func configureSortTypeButton(type: ReviewSortType) {
         var configuration = UIButton.Configuration.plain()
+        
         var text = AttributedString(type.koreanDescription)
         text.font = UIFont.appFont(.pretendardRegular, size: 14)
         configuration.attributedTitle = text
+        
         let imageSize = CGSize(width: 15, height: 12)
         let image = UIImage(systemName: SFSymbols.chevronDown.rawValue)
         let resizedImage = image?.withConfiguration(
@@ -94,6 +103,7 @@ final class ReviewListHeaderView: UICollectionReusableView {
         configuration.image = resizedImage
         configuration.imagePlacement = .trailing
         configuration.imagePadding = 4
+        
         configuration.baseBackgroundColor = .systemBackground
         configuration.baseForegroundColor = UIColor.appColor(.neutral500)
         configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
@@ -101,7 +111,7 @@ final class ReviewListHeaderView: UICollectionReusableView {
         sortTypeButton.configuration = configuration
     }
     
-    private func setUpMyReviewButtonText() {
+    private func configureMyReviewButton() {
         var configuration = UIButton.Configuration.plain()
         let imageSize = CGSize(width: 14.17, height: 14.17)
         let image = myReviewButton.isSelected
@@ -111,11 +121,12 @@ final class ReviewListHeaderView: UICollectionReusableView {
         let resizedImage = image?.withConfiguration(
             UIImage.SymbolConfiguration(pointSize: imageSize.width, weight: .regular)
         )
-        
         configuration.image = resizedImage
+        
         var text = AttributedString("내가 작성한 리뷰")
         text.font = UIFont.appFont(.pretendardRegular, size: 14)
         configuration.attributedTitle = text
+        
         configuration.imagePadding = 4
         configuration.baseBackgroundColor = .systemBackground
         configuration.baseForegroundColor = UIColor.appColor(.neutral500)
@@ -135,34 +146,56 @@ final class ReviewListHeaderView: UICollectionReusableView {
         sortTypeButton.configuration = configuration
     }
 }
+
+// MARK: - @objc
 extension ReviewListHeaderView {
     
-    private func setupViews() {
-        [separateView, sortTypeButton, myReviewButton].forEach { component in
-            addSubview(component)
-        }
-        
-        separateView.snp.makeConstraints { make in
-            make.top.equalTo(self.snp.top)
-            make.leading.equalTo(self.snp.leading).offset(24)
-            make.trailing.equalTo(self.snp.trailing).offset(-16)
-            make.height.equalTo(1)
-        }
-        
-        sortTypeButton.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.leading.equalTo(self.snp.leading).offset(24)
-            make.width.equalTo(100)
-            make.height.equalTo(22)
-        }
-        
-        myReviewButton.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.trailing.equalTo(self.snp.trailing).offset(-24)
-            make.width.equalTo(120)
-            make.height.equalTo(22)
-        }
-        
+    @objc private func sortTypeButtonTapped() {
+        updateSortTypeButtonImage(for: .chevronUp)
+        dropDown.show()
     }
     
+    @objc private func myReviewButtonTapped() {
+        myReviewButton.isSelected.toggle()
+        configureMyReviewButton()
+        myReviewButtonPublisher.send(myReviewButton.isSelected)
+    }
+}
+
+extension ReviewListHeaderView {
+    
+    private func setUpLayout() {
+        [separateView, sortTypeButton, myReviewButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        separateView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(24)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.height.equalTo(1)
+        }
+        
+        sortTypeButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(24)
+            $0.width.equalTo(100)
+            $0.height.equalTo(22)
+        }
+        
+        myReviewButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-24)
+            $0.width.equalTo(120)
+            $0.height.equalTo(22)
+        }
+    }
+    
+    private func configureView() {
+        setUpLayout()
+        setupConstraints()
+        self.backgroundColor = UIColor.appColor(.newBackground)
+    }
 }
