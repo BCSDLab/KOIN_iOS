9f7cedafef0a8ec07b45c02f79bcbda1fffbdd9a
hgjwilly-koreatech <hgjwilly@gmail.com>
2025-10-24 18:40:43 +0900

    chore: 각주 추가
    FetchCartUseCase의 로직에 각주를 추가했습니다.

--- Koin/Domain/UseCase/Order/FetchCartUseCase.swift
+++ Koin/Domain/UseCase/Order/FetchCartUseCase.swift
@@ -20,6 +20,10 @@ final class DefaultFetchCartUseCase: FetchCartUseCase {
     }
     
     func execute() -> AnyPublisher<(Cart, isFromDelivery: Bool), Error> {
+        /// DELIVERY 파라미터로 우선 시도합니다
+        /// 잘못된 파라미터 오류시, TAKE_OUT 파라미터로 재시도합니다
+        /// 두 파라미터 모두 실패시, 장바구니 정보 호출 실패합니다.
+        /// 어떤 파라미터로 호출하여 설정했는지 알기 위해, 반환타입을 튜플로 바꿉니다.
         repository.fetchCart(parameter: .delivery)
             .map {
                 return ($0, isFromDelivery: true)
