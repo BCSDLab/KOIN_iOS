9d7792a7444b665320902dbf846307ac3e3a0961
GI JEONG HONG <hgjwilly@gmail.com>
2025-11-07 18:12:10 +0900

    add: 메뉴그룹과 메뉴를 함께 반환하는 UseCase 추가
    주변상점의 메뉴그룹을 반환하는 UseCase, 메뉴를 반환하는 UseCase가 별도로 있었습니다.
    메뉴 api 호출결과에서 메뉴그룹을 꺼내서, 메뉴그룹과 메뉴를 함께 반환해주는 UseCase를 만들었습니다.
    기존의 두 UseCase를 새로운 하나의 UseCase를 대체합니다.

---
+++ Koin/Domain/UseCase/Shop/FetchOrderShopMenusAndMenusGroupsFromShopUseCase.swift
@@ -0,0 +1,36 @@
+//
+//  FetchOrderShopMenusAndMenusGroupsFromShopUseCase.swift
+//  koin
+//
+//  Created by 홍기정 on 11/7/25.
+//
+
+import Combine
+
+protocol FetchOrderShopMenusAndGroupsFromShopUseCase {
+    func execute(shopId: Int) -> AnyPublisher<(OrderShopMenusGroups, [OrderShopMenus]), Error>
+}
+
+final class DefaultFetchOrderShopMenusAndGroupsFromShopUseCase: FetchOrderShopMenusAndGroupsFromShopUseCase {
+    
+    private let shopRepository: ShopRepository
+    
+    init(shopRepository: ShopRepository) {
+        self.shopRepository = shopRepository
+    }
+    
+    func execute(shopId: Int) -> AnyPublisher<(OrderShopMenusGroups, [OrderShopMenus]), Error> {
+        return shopRepository.fetchShopMenuList(requestModel: FetchShopDataRequest(shopId: shopId)).map { menuDto in
+            let orderShopMenus: [OrderShopMenus] = (menuDto.menuCategories ?? []).map {
+                OrderShopMenus(from: $0)
+            }
+            let menuGroup: [MenuGroup] = (menuDto.menuCategories ?? []).map {
+                return MenuGroup(name: $0.name)
+            }
+            let orderShopMenuGroups = OrderShopMenusGroups(menuGroups: menuGroup)
+            
+            return (orderShopMenuGroups, orderShopMenus)
+        }
+        .eraseToAnyPublisher()
+    }
+}
