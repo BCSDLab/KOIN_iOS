cad6a980ce3ea91496d26d1a9c88c1e20e9ffa2b
oeunji <oieunxe@gmail.com>
2025-11-09 19:21:38 +0900

    feat: 라디오 버튼 컬러 정의를 위한 구조체 구현

--- Koin/Core/DesignSystem/RadioButton/RadioButtonColors.swift
+++ Koin/Core/DesignSystem/RadioButton/RadioButtonColors.swift
@@ -5,4 +5,11 @@
 //  Created by 이은지 on 11/9/25.
 //
 
-import Foundation
+import UIKit
+
+struct RadioButtonColors {
+    let border: UIColor
+    let background: UIColor
+    let innerCircle: UIColor
+    let text: UIColor
+}
