e8d25b6d4b163b46b5815404896d56ac6b2f8843
oeunji <oieunxe@gmail.com>
2025-11-09 20:04:51 +0900

    delete: 불필요한 코드 파일 삭제

--- Koin/Core/DesignSystem/RadioButton/RadioButtonGroup.swift
+++ Koin/Core/DesignSystem/RadioButton/RadioButtonGroup.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+final class RadioButtonGroup {
+    
+    private var radioButtons: [RadioButton] = []
+    var selectedButton: RadioButton?
+    
+    var onSelectionChanged: ((RadioButton?) -> Void)?
+    
+    func addButton(_ button: RadioButton) {
+        radioButtons.append(button)
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .valueChanged)
+    }
+    
+    @objc private func buttonTapped(_ button: RadioButton) {
+        if button.isSelected {
+            radioButtons.forEach {
+                if $0 != button {
+                    $0.isSelected = false
+                }
+            }
+            selectedButton = button
+        } else {
+            if selectedButton == button {
+                selectedButton = nil
+            }
+        }
+        onSelectionChanged?(selectedButton)
+    }
+    
+    func deselectAll() {
+        radioButtons.forEach { $0.isSelected = false }
+        selectedButton = nil
+    }
+}
