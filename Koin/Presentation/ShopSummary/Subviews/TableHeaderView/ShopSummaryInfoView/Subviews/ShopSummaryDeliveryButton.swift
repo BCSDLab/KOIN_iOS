489be4ba8f4a649d9a7ce5ffcd279d8c547ce5f6
GI JEONG HONG <hgjwilly@gmail.com>
2025-11-15 16:24:10 +0900

    chore: Subview 분리
    CustomButton을 DelieveryButton과 IntroductionButton으로 분리했습니다.

--- Koin/Presentation/ShopSummary/Subviews/TableHeaderView/ShopSummaryInfoView/Subviews/ShopSummaryDeliveryButton.swift
+++ Koin/Presentation/ShopSummary/Subviews/TableHeaderView/ShopSummaryInfoView/Subviews/ShopSummaryDeliveryButton.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ShopSummaryCustomButton: UIButton {
+final class ShopSummaryDeliveryButton: UIButton {
     
     // MARK: - UI Components
     private let minimumOrderLabel = UILabel().then {
@@ -23,11 +23,6 @@ final class ShopSummaryCustomButton: UIButton {
         $0.font = UIFont.appFont(.pretendardRegular, size: 12)
         $0.textColor = UIColor.appColor(.neutral800)
         $0.contentMode = .center
-    }
-    private let introductionLabel = UILabel().then {
-        $0.numberOfLines = 2
-        $0.font = UIFont.appFont(.pretendardRegular, size: 12)
-        $0.textColor = UIColor.appColor(.neutral800)
     }
     private let minimumOrderSubLabel = UILabel().then {
         $0.numberOfLines = 1
@@ -47,9 +42,9 @@ final class ShopSummaryCustomButton: UIButton {
         $0.font = .appFont(.pretendardSemiBold, size: 12)
         $0.textColor = .appColor(.neutral400)
         $0.textAlignment = .center
+        $0.isHidden = true
     }
     
-    private let leftImageView = UIImageView(image: UIImage.appImage(asset: .speaker))
     private let rightImageView = UIImageView(image: UIImage.appImage(asset: .newChevronRight)?.withRenderingMode(.alwaysTemplate)).then {
         $0.tintColor = .appColor(.neutral500)
     }
@@ -62,15 +57,9 @@ final class ShopSummaryCustomButton: UIButton {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    func configure(introduction: String?) {
-        introductionLabel.setLineHeight(lineHeight: 1.6, text: introduction ?? "공지사항")
-        configureIntroductionView()
-    }
+    
     func configure(minOrderAmount: Int, minDeliveryTip: Int, maxDelieveryTip: Int, isFromOrder: Bool) {
-        //if isFromOrder {
         
-        configureOrderAmountDelieveryTipView()
         minimumOrderSubLabel.text = "\(minOrderAmount.formattedWithComma)원"
         
         if minDeliveryTip == 0, maxDelieveryTip == 0 {
@@ -80,29 +69,28 @@ final class ShopSummaryCustomButton: UIButton {
             deliveryTipSubLabel.text = "\(minDeliveryTip.formattedWithComma) - \(maxDelieveryTip.formattedWithComma)원"
         }
         
-        //}
-        //else {
-        //    configureInorderableLabel()
+        //if isFromOrder {
+        // setUpIsHidden()
         //}
     }
 }
 
-extension ShopSummaryCustomButton {
+extension ShopSummaryDeliveryButton {
     
-    private func configureInorderableLabel() {
-        [inorderableLabel].forEach {
-            addSubview($0)
+    private func setUpIsHidden() {
+        [minimumOrderLabel, minimumOrderSubLabel, deliveryTipLabel, deliveryTipSubLabel, rightImageView].forEach {
+            $0.isHidden = true
         }
-        inorderableLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview().offset(-4)
+        inorderableLabel.isHidden = false
+    }
+    
+    private func setUpLayout() {
+        [minimumOrderLabel, minimumOrderSubLabel, deliveryTipLabel, deliveryTipSubLabel, rightImageView, inorderableLabel].forEach {
+            addSubview($0)
         }
     }
-
-    private func configureOrderAmountDelieveryTipView() {
-        [minimumOrderLabel, minimumOrderSubLabel, deliveryTipLabel, deliveryTipSubLabel, rightImageView].forEach {
-            addSubview($0)
-        }
+    
+    private func setUpConstraints() {
         
         minimumOrderLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(12)
@@ -129,30 +117,16 @@ extension ShopSummaryCustomButton {
             $0.centerY.equalToSuperview()
             $0.width.height.equalTo(20)
         }
-    }
-    
-    private func configureIntroductionView() {
-        [leftImageView, introductionLabel, rightImageView].forEach {
-            addSubview($0)
-        }
-        
-        leftImageView.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(12)
-            $0.centerY.equalToSuperview()
-        }
-        introductionLabel.snp.makeConstraints {
-            $0.leading.equalTo(leftImageView.snp.trailing).offset(7)
-            $0.trailing.equalTo(rightImageView.snp.leading).offset(-7)
+        inorderableLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
             $0.centerY.equalToSuperview().offset(-4)
-        }
-        rightImageView.snp.makeConstraints {
-            $0.trailing.equalToSuperview().offset(-12)
-            $0.centerY.equalToSuperview()
-            $0.width.height.equalTo(20)
         }
     }
     
     private func configureView() {
+        setUpLayout()
+        setUpConstraints()
+        
         backgroundColor = .appColor(.neutral0)
         layer.cornerRadius = 12
     }
