70058f11485cb448dec6128ed520157a0e32f3bb
GI JEONG HONG <hgjwilly@gmail.com>
2025-12-18 05:31:24 +0900

    chore: 사용하지 않는 파일 삭제, 디렉토리 이동
    사용하지 않는 파일을 삭제했습니다.
    Order 화면의 SubViews를 Shop 화면에서 사용하는 경우, 디렉토리를 이동했습니다.

---
+++ Koin/Presentation/Shop/Shop/SubViews/OrderCategoryCollectionView/OrderCategoryCollectionViewCell.swift
@@ -0,0 +1,79 @@
+//
+//  OrderCategoryCollectionViewCell.swift
+//  koin
+//
+//  Created by 이은지 on 6/20/25.
+//
+
+import UIKit
+import SnapKit
+
+final class OrderCategoryCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - UI Component
+    private let indicatorView = UIView().then {
+        $0.backgroundColor = UIColor.appColor(.neutral300)
+        $0.layer.cornerRadius = 24
+    }
+    
+    private let imageView = UIImageView().then {
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+    }
+
+    private let titleLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.font = UIFont.appFont(.pretendardRegular, size: 12)
+        $0.lineBreakMode = .byClipping
+        $0.sizeToFit()
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(info: ShopCategory, _ selected: Bool) {
+        imageView.loadImage(from: info.imageURL)
+        titleLabel.text = info.name
+        indicatorView.isHidden = !selected
+    }
+}
+
+extension OrderCategoryCollectionViewCell {
+    private func setUpLayouts() {
+        [indicatorView, imageView, titleLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setUpConstraints() {
+        indicatorView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.width.height.equalTo(48)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.centerX.equalTo(indicatorView)
+            $0.centerY.equalTo(indicatorView)
+            $0.width.height.equalTo(32)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(8)
+            $0.height.equalTo(19)
+            $0.centerX.equalTo(imageView.snp.centerX)
+        }
+    }
+    
+    private func configureView() {
+        setUpLayouts()
+        setUpConstraints()
+    }
+}
