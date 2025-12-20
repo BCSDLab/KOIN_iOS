336c5771658c54e84c1f80d731849f952cbf5183
xp65241 <xp65241@naver.com>
2025-12-20 21:17:43 +0900

    feat: HomeCoordinator 구현

--- Koin/Presentation/Home/Home/HomeCoordinator.swift
+++ Koin/Presentation/Home/Home/HomeCoordinator.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+final class HomeCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    weak var parentCoordinator: Coordinator?
+    
+    private let factory: HomeFactory
+        
+    init(navigationController: UINavigationController, factory: HomeFactory) {
+        self.navigationController = navigationController
+        self.factory = factory
+    }
+
+    func start() {
+        let homeViewController = factory.makeHomeViewController()
+        homeViewController.coordinator = self
+        navigationController.setViewControllers([homeViewController], animated: false)
+    }
+}
