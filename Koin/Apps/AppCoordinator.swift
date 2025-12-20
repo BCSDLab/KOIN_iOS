6e85a7cbd96fa2a755b49c321f7877e2cbdaf5e8
xp65241 <xp65241@naver.com>
2025-12-20 21:17:18 +0900

    feat: AppCoordinator 구현

--- Koin/Apps/AppCoordinator.swift
+++ Koin/Apps/AppCoordinator.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var parentCoordinator: Coordinator? = nil
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let homeCoordinator = HomeCoordinator(
+            navigationController: navigationController,
+            factory: DIContainer.shared
+        )
+        
+        addChild(homeCoordinator)
+        homeCoordinator.parentCoordinator = self
+        homeCoordinator.start()
+    }
+}
