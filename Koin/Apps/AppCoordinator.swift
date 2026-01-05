78fe2c812daccad1805f13768a77cddf44c63cce
xp65241 <xp65241@naver.com>
2026-01-05 14:10:39 +0900

    add: 코디네이터 프로토콜 구현

---
+++ Koin/Apps/AppCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppCoordinator.swift
+//  koin
+//
+//  Created by 김성민 on 12/20/25.
+//
+
+import Foundation
--- Koin/Core/Protocol/Coordinator.swift
+++ Koin/Core/Protocol/Coordinator.swift
@@ -9,6 +9,19 @@ import UIKit
 
 protocol Coordinator: AnyObject {
     var parentCoordinator: Coordinator? { get set }
-    var children: [Coordinator] { get set }
+    var childCoordinators: [Coordinator] { get set }
     var navigationController: UINavigationController { get set }
+    
+    func start()
+}
+
+extension Coordinator {
+    func addChild(_ child: Coordinator) {
+        childCoordinators.append(child)
+    }
+    
+    func removeChild(_ child: Coordinator?) {
+        guard let child = child else { return }
+        childCoordinators = childCoordinators.filter { $0 !== child }
+    }
 }
