148b4377cc661613059420fc0db79c03c66963d1
GI JEONG HONG <hgjwilly@gmail.com>
2026-01-31 19:21:56 +0900

    design: 홈화면 분실물 UI 변경

--- Koin/Presentation/Home/Home/SubViews/LostItemListView.swift
+++ Koin/Presentation/Home/Home/SubViews/LostItemListView.swift
@@ -23,16 +23,21 @@ final class LostItemListView: UIView {
         $0.font = UIFont.appFont(.pretendardBold, size: 15)
     }
     
-    private let chevronButton = UIButton().then {
-        $0.setImage(UIImage(named: "chevronRightBlue")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        $0.tintColor = UIColor(hexCode: "1C1B1F")
-    }
-    
     private let descriptionButton = UIButton().then {
         $0.backgroundColor = .appColor(.neutral100)
         $0.layer.cornerRadius = 8
         $0.clipsToBounds = true
     }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.font = .appFont(.pretendardMedium, size: 14)
+        $0.textColor = .appColor(.neutral800)
+    }
+    
+    private let chevronImageView = UIImageView(image: .appImage(asset: .chevronRightBlue)?.withTintColor(UIColor(hexCode: "1C1B1F"))
+    )
+    
+    
     
     // MARK: - Initializer
     init() {
@@ -47,16 +52,25 @@ final class LostItemListView: UIView {
     func configure(lostItemStats: LostItemStats) {
         foundCountTitle = "지금까지 \(lostItemStats.foundCount)명이 분실물을 찾았어요."
         notFoundCountTitle = "\(lostItemStats.notFoundCount)개의 분실물이 주인을 찾고있어요."
-        descriptionButton.setAttributedTitle(NSAttributedString(
-            string: notFoundCountTitle,
-            attributes: [
-                .font : UIFont.appFont(.pretendardMedium, size: 14),
-                .foregroundColor : UIColor.appColor(.neutral800)
-            ]), for: .normal
-        )
+        
+        let longerTitle: String
+        foundCountTitle.count < notFoundCountTitle.count ? (longerTitle = notFoundCountTitle) : (longerTitle = foundCountTitle)
+        
+        descriptionLabel.text = longerTitle
+        
+        descriptionLabel.snp.remakeConstraints {
+            $0.center.equalTo(descriptionButton)
+            $0.width.equalTo(descriptionLabel.intrinsicContentSize.width)
+        }
+        chevronImageView.snp.remakeConstraints {
+            $0.centerY.equalTo(descriptionLabel)
+            $0.leading.equalTo(descriptionLabel.snp.trailing).offset(8)
+        }
         
         if 50 <= lostItemStats.foundCount {
             setTimer()
+        } else {
+            descriptionLabel.text = notFoundCountTitle
         }
     }
 }
@@ -73,24 +87,16 @@ extension LostItemListView {
                 $0.type = .push
                 $0.subtype = .fromTop
             }
-            descriptionButton.titleLabel?.layer.add(transition, forKey: "newTitle")
+            descriptionLabel.layer.add(transition, forKey: "newTitle")
             
-            let newTitle = descriptionButton.titleLabel?.text == notFoundCountTitle ? foundCountTitle : notFoundCountTitle
+            let newTitle = descriptionLabel.text == notFoundCountTitle ? foundCountTitle : notFoundCountTitle
             
-            descriptionButton.setAttributedTitle(NSAttributedString(
-                string: newTitle,
-                attributes: [
-                    .font : UIFont.appFont(.pretendardMedium, size: 14),
-                    .foregroundColor : UIColor.appColor(.neutral800)
-                ]), for: .normal
-            )
+            descriptionLabel.text = newTitle
         }
     }
     
     private func setAddTargets() {
-        [chevronButton, descriptionButton].forEach {
-            $0.addTarget(self, action: #selector(lostItemListTapped), for: .touchUpInside)
-        }
+        descriptionButton.addTarget(self, action: #selector(lostItemListTapped), for: .touchUpInside)
     }
     @objc private func lostItemListTapped() {
         lostItemListTappedPublisher.send()
@@ -99,7 +105,7 @@ extension LostItemListView {
 extension LostItemListView {
     
     private func setLayouts() {
-        [nameLabel, chevronButton, descriptionButton].forEach {
+        [nameLabel, descriptionButton, descriptionLabel, chevronImageView].forEach {
             addSubview($0)
         }
     }
@@ -108,16 +114,18 @@ extension LostItemListView {
             $0.top.equalToSuperview()
             $0.leading.equalToSuperview().offset(20)
         }
-        chevronButton.snp.makeConstraints {
-            $0.centerY.equalTo(nameLabel)
-            $0.trailing.equalToSuperview().offset(-24)
-            $0.size.equalTo(29)
-        }
         descriptionButton.snp.makeConstraints {
             $0.top.equalTo(nameLabel.snp.bottom).offset(16)
             $0.leading.trailing.equalToSuperview().inset(24)
             $0.height.equalTo(44)
             $0.bottom.equalToSuperview()
+        }
+        descriptionLabel.snp.makeConstraints {
+            $0.center.equalTo(descriptionButton)
+        }
+        chevronImageView.snp.makeConstraints {
+            $0.centerY.equalTo(descriptionLabel)
+            $0.centerX.equalTo(descriptionLabel).offset(100)
         }
     }
     private func configureView() {
