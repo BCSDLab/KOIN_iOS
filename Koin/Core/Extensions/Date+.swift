7a566495983a7b22d3294e6c912ce3740b570b1b
GI JEONG HONG <hgjwilly@gmail.com>
2026-03-08 17:46:20 +0900

    add: Date extension 추가

--- Koin/Core/Extensions/Date+.swift
+++ Koin/Core/Extensions/Date+.swift
@@ -9,6 +9,18 @@ import Foundation
 
 extension Date {
     
+    var year: Int {
+        return Calendar.current.component(.year, from: self)
+    }
+    
+    var month: Int {
+        return Calendar.current.component(.month, from: self)
+    }
+    
+    var day: Int {
+        return Calendar.current.component(.day, from: self)
+    }
+    
     func isWeekend() -> Bool {
         let calendar = Calendar.current
         let components = calendar.dateComponents([.weekday], from: self)
