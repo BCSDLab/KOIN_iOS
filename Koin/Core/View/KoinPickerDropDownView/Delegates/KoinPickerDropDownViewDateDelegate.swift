22ddc898e9a11b815ad6d3bc76d536ac3888b73b
GI JEONG HONG <hgjwilly@gmail.com>
2026-03-08 17:53:12 +0900

    design: 콜밴팟 등록하기 UI 구현

---
+++ Koin/Core/View/KoinPickerDropDownView/Delegates/KoinPickerDropDownViewDateDelegate.swift
@@ -0,0 +1,92 @@
+//
+//  KoinPickerDropDownViewDateDelegate.swift
+//  koin
+//
+//  Created by 홍기정 on 3/6/26.
+//
+
+import Foundation
+
+final class KoinPickerDropDownViewDateDelegate {
+    
+    // MARK: - Properties
+    let columnWidths: [CGFloat] = [53, 31, 31]
+    
+    private let inputFormatter = DateFormatter().then {
+        $0.dateFormat = "yyyy-MM-dd"
+    }
+    
+    private let outputFormatter = DateFormatter().then {
+        $0.dateFormat = "yyyy년*M월*d일"
+    }
+}
+
+extension KoinPickerDropDownViewDateDelegate: KoinPickerDropDownViewDelegate {
+    
+    func reset(koinPicker: KoinPickerDropDownView) {
+        reset(koinPicker: koinPicker, initialDate: Date())
+    }
+    
+    func reset(koinPicker: KoinPickerDropDownView, initialDate: Date) {
+        
+        let selectedItem = outputFormatter.string(from: initialDate).components(separatedBy: "*")
+        let items = getItems(selectedItem: selectedItem)
+        
+        koinPicker.reset(items: items, selectedItem: selectedItem, columnWidths: [53, 31, 31])
+    }
+
+    func selectedItemUpdated(koinPicker: KoinPickerDropDownView, selectedItem: [String]) {
+        
+        guard let yearInt = Int(selectedItem[0].filter { $0.isNumber }),
+              let monthInt = Int(selectedItem[1].filter { $0.isNumber }),
+              var dayInt = Int(selectedItem[2].filter { $0.isNumber }) else {
+            assert(false)
+            return
+        }
+        let maxDay = getAllDays(year: yearInt, month: monthInt).count
+        if dayInt > maxDay {
+            dayInt = maxDay
+        }
+        guard let validDate = inputFormatter.date(from: String(format: "%d-%d-%d", yearInt, monthInt, dayInt)) else {
+            assert(false)
+        }
+        reset(koinPicker: koinPicker, initialDate: validDate)
+    }
+}
+
+extension KoinPickerDropDownViewDateDelegate {
+    
+    private func getItems(selectedItem: [String]) -> [[String]] {
+        
+        guard let selectedYearInt = Int(selectedItem[0].filter { $0.isNumber }),
+              let selectedMonthInt = Int(selectedItem[1].filter { $0.isNumber }) else {
+            assert(false)
+        }
+        
+        let years: [String] = {
+            let thisYearInt = Date().year
+            let lowestYearInt = min(selectedYearInt, thisYearInt)
+            let heighestYearInt = thisYearInt + 1
+            return Range(lowestYearInt...heighestYearInt).map { String($0)+"년" }
+        }()
+        let months: [String] = Range(1...12).map { String($0)+"월" }
+        let days: [String] = getAllDays(year: selectedYearInt, month: selectedMonthInt)
+        
+        return [years, months, days]
+    }
+    
+    func getAllDays(year: Int, month: Int) -> [String] {
+        
+        let calendar = Calendar.current
+        var components = DateComponents()
+        components.year = year
+        components.month = month
+        
+        guard let date = calendar.date(from: components),
+              let dayRange = calendar.range(of: .day, in: .month, for: date) else {
+            assert(false)
+        }
+        
+        return dayRange.map { String($0) + "일" }
+    }
+}
---
+++ Koin/Presentation/CallVan/CallVanList/Subviews/CallVanListCollectionView/CallVanButton.swift
@@ -0,0 +1,54 @@
+//
+//  CallVanButton.swift
+//  koin
+//
+//  Created by 홍기정 on 3/3/26.
+//
+
+import UIKit
+
+final class CallVanButton: UIButton {
+    
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    func configure(state: CallVanState) {
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.attributedTitle = AttributedString(
+            state.rawValue,
+            attributes: AttributeContainer([
+                .font : UIFont.appFont(.pretendardMedium, size: 14),
+                .foregroundColor : state.foregroundColor
+            ])
+        )
+        
+        let inset: CGFloat
+        switch state {
+        case .재모집, .이용완료:
+            inset = 8
+        default:
+            inset = 12
+        }
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: inset, bottom: 0, trailing: inset)
+        
+        self.configuration = configuration
+        
+        backgroundColor = state.backgroundColor
+        layer.cornerRadius = 4
+        if let _ = state.borderColor {
+            layer.borderWidth = 1
+            layer.borderColor = state.borderColor?.cgColor
+        }
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let contentSize = super.intrinsicContentSize
+        return CGSize(width: contentSize.width, height: 30)
+    }
+}
