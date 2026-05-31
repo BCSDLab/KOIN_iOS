6e3b84f62e9cb114e2f065e22fca7f80e1b17d93
GI JEONG HONG <hgjwilly@gmail.com>
2026-05-31 21:11:47 +0900

    refactor: UIKit/SwiftUI 공용 Asset enum 분리

--- Koin/Core/Extensions/UIKit/UIFont+.swift
+++ Koin/Core/Extensions/UIKit/UIFont+.swift
@@ -7,25 +7,9 @@
 
 import UIKit.UIFont
 
-public enum FontAsset {
-    case pretendardRegular
-    case pretendardMedium
-    case pretendardBold
-    case pretendardSemiBold
-}
-
 extension UIFont {
     static func appFont(_ name: FontAsset, size: Int) -> UIFont {
         let fontSize = CGFloat(size)
-        switch name {
-        case .pretendardRegular:
-            return UIFont(name: "Pretendard-Regular", size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
-        case .pretendardMedium:
-            return UIFont(name: "Pretendard-Medium", size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
-        case .pretendardBold:
-            return UIFont(name: "Pretendard-Bold", size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
-        case .pretendardSemiBold:
-            return UIFont(name: "Pretendard-SemiBold", size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
-        }
+        return UIFont(name: name.rawValue, size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
     }
 }
