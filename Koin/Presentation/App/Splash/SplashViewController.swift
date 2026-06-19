6c452f61da055fc8e8df417eb4e39feead1cebb9
GI JEONG HONG <hgjwilly@gmail.com>
2026-06-19 15:27:49 +0900

    refactor: SplashViewController 추가
    AppCoordinator가 앱 버전 체크, 사용자 정보 체크를 하는 동안 표시할 스플래시 화면을 추가합니다.

---
+++ Koin/Presentation/App/Splash/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  koin
+//
+//  Created by 홍기정 on 4/23/26.
+//
+
+import UIKit
+
+final class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
