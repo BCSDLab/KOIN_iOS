b2c8b91e642653d104c3e5d04b6b5d81c4b4eb20
GI JEONG HONG <hgjwilly@gmail.com>
2026-07-07 19:18:56 +0900

    feat: 알림화면 화면 전환 구현

--- Koin/Presentation/NewHome/Notification/NotificationViewController.swift
+++ Koin/Presentation/NewHome/Notification/NotificationViewController.swift
@@ -86,10 +86,11 @@ private extension NotificationViewController {
             }
             .store(in: &subscriptions)
         
-        notificationTableView.tapNotificationPublisher // TODO: Navigation
+        notificationTableView.tapNotificationPublisher
             .sink { [weak self] item in
                 self?.inputSubject.send(.markAsRead(id: item.id))
                 self?.inputSubject.send(.logEvent(EventParameter.EventLabel.Campus.notificationList, .click, item.title))
+                self?.handleNavigation(item)
             }
             .store(in: &subscriptions)
     }
@@ -119,6 +120,196 @@ private extension NotificationViewController {
             self?.emptyView.isHidden = !shouldShowEmpty
             self?.notificationTableView.isHidden = shouldShowEmpty
         }
+    }
+}
+
+// MARK: - Navigation
+
+extension NotificationViewController {
+    private func handleNavigation(_ item: NotificationItem) {
+        guard let uri: String = item.uri,
+              let parsedQuery = parseQuery(uri: uri) else {
+            return
+        }
+        switch item.appPath {
+        case .shop:
+            if let shopIdString = parsedQuery["id"],
+               let shopIdInt = Int(shopIdString) {
+                navigateToShop(shopId: shopIdInt)
+            }
+        case .dining:
+            navigateToDining()
+        case .chat:
+            if let articleIdString = parsedQuery["articleId"],
+               let chatRoomIdString = parsedQuery["chatRoomId"],
+               let articleIdInt = Int(articleIdString),
+               let chatRoomIdInt = Int(chatRoomIdString) {
+                navigateToChat(articleId: articleIdInt, chatRoomId: chatRoomIdInt)
+            }
+        case .callvan:
+            if let postIdString = parsedQuery["postId"],
+               let postIdInt = Int(postIdString) {
+                navigateToCallVanData(postId: postIdInt)
+            }
+        case .callvanChat:
+            if let postIdString = parsedQuery["postId"],
+               let chatRoomIdString = parsedQuery["chatRoomId"],
+               let postIdInt = Int(postIdString),
+               let chatRoomIdInt = Int(chatRoomIdString) {
+                navigateToChat(postId: postIdInt, chatRoomId: chatRoomIdInt)
+            }
+        case .keyword:
+            if let noticeIdString = parsedQuery["id"],
+               let keyword = parsedQuery["keyword"],
+               let boardIdString = parsedQuery["board-id"],
+               let noticeIdInt = Int(noticeIdString),
+               let boardIdInt = Int(boardIdString) {
+                navigateToKeyword(boardId: boardIdInt, noticeId: noticeIdInt)
+            }
+        default:
+            break
+        }
+    }
+    
+    private func parseQuery(uri: String) -> [String: String]? {
+        if let components = URLComponents(string: uri),
+           let qureyItems = components.queryItems {
+            return qureyItems.reduce(into: [:]) { result, item in
+                result[item.name] = item.value
+            }
+        } else {
+            return nil
+        }
+    }
+    
+    private func navigateToShop(shopId: Int) {
+        let viewController = makeShopViewController()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func navigateToDining() {
+        let viewController = makeDiningViewController()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func navigateToChat(articleId: Int, chatRoomId: Int) {
+        let viewModel = ChatViewModel(articleId: articleId, chatRoomId: chatRoomId, articleTitle: nil)
+        let viewController = ChatViewController(viewModel: viewModel)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func navigateToCallVanData(postId: Int) {
+        let viewController = makeCallVanDataViewController(postId: postId)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func navigateToChat(postId: Int, chatRoomId: Int) {
+        let viewController = makeCallVanChatViewController(postId: postId)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func navigateToKeyword(boardId: Int, noticeId: Int) {
+        let viewController = makeNoticeDataViewController(noticeId: noticeId, boardId: boardId)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func makeCallVanDataViewController(postId: Int) -> CallVanDataViewController {
+        let callVanRepository = DefaultCallVanRepository(service: DefaultCallVanService())
+        let fetchCallVanDataUseCase = DefaultFetchCallVanDataUseCase(repository: callVanRepository)
+        let fetchCallVanNotificationListUseCase = DefaultFetchCallVanNotificationListUseCase(repository: callVanRepository)
+        let logAnalyticsEventUseCase = DefaultLogAnalyticsEventUseCase(repository: GA4AnalyticsRepository(service: GA4AnalyticsService()))
+        let viewModel = CallVanDataViewModel(
+            postId: postId,
+            fetchCallVanDataUseCase: fetchCallVanDataUseCase,
+            fetchCallVanNotificationListUseCase: fetchCallVanNotificationListUseCase,
+            logAnalyticsEventUseCase: logAnalyticsEventUseCase
+        )
+        return CallVanDataViewController(viewModel: viewModel)
+    }
+    
+    private func makeCallVanChatViewController(postId: Int) -> CallVanChatViewController {
+        let callVanRepository = DefaultCallVanRepository(service: DefaultCallVanService())
+        let coreRepository = DefaultCoreRepository(service: DefaultCoreService())
+        let fetchCallVanChatUseCase = DefaultFetchCallVanChatUseCase(repository: callVanRepository)
+        let postCallVanChatUseCase = DefaultPostCallVanChatUseCase(repository: callVanRepository)
+        let fetchCallVanDataUseCase = DefaultFetchCallVanDataUseCase(repository: callVanRepository)
+        let uploadFileUseCase = DefaultUploadFileUseCase(coreRepository: coreRepository)
+        let logAnalyticsEventUseCase = DefaultLogAnalyticsEventUseCase(repository: GA4AnalyticsRepository(service: GA4AnalyticsService()))
+        let viewModel = CallVanChatViewModel(
+            postId: postId,
+            fetchCallVanChatUseCase: fetchCallVanChatUseCase,
+            postCallVanChatUseCase: postCallVanChatUseCase,
+            fetchCallVanDataUseCase: fetchCallVanDataUseCase,
+            uploadFileUseCase: uploadFileUseCase,
+            logAnalyticsEventUseCase: logAnalyticsEventUseCase
+        )
+        return CallVanChatViewController(viewModel: viewModel)
+    }
+    
+    private func makeNoticeDataViewController(noticeId: Int, boardId: Int) -> NoticeDataViewController {
+        let service = DefaultNoticeService()
+        let repository = DefaultNoticeListRepository(service: service)
+        let viewModel = NoticeDataViewModel(
+            fetchNoticeDataUseCase: DefaultFetchNoticeDataUseCase(noticeListRepository: repository),
+            fetchHotNoticeArticlesUseCase: DefaultFetchHotNoticeArticlesUseCase(noticeListRepository: repository),
+            downloadNoticeAttachmentUseCase: DefaultDownloadNoticeAttachmentsUseCase(noticeRepository: repository),
+            logAnalyticsEventUseCase: DefaultLogAnalyticsEventUseCase(repository: GA4AnalyticsRepository(service: GA4AnalyticsService())),
+            noticeId: noticeId,
+            boardId: boardId
+        )
+        return NoticeDataViewController(viewModel: viewModel)
+    }
+    
+    private func makeDiningViewController() -> DiningViewController {
+        let diningService = DefaultDiningService()
+        let shareService = KakaoShareService()
+        let diningRepository = DefaultDiningRepository(diningService: diningService, shareService: shareService)
+        let notiRepository = DefaultNotiRepository(service: DefaultNotiService())
+        let fetchDiningListUseCase = DefaultFetchDiningListUseCase(diningRepository: diningRepository)
+        let diningLikeUseCase = DefaultDiningLikeUseCase(diningRepository: diningRepository)
+        let logAnalyticsEventUseCase = DefaultLogAnalyticsEventUseCase(repository: GA4AnalyticsRepository(service: GA4AnalyticsService()))
+        let dateProvider = DefaultDateProvider()
+        let shareMenuListUseCase = DefaultShareMenuListUseCase(diningRepository: diningRepository)
+        let changeNotiUseCase = DefaultChangeNotiUseCase(notiRepository: notiRepository)
+        let changeNotiDetailUseCase = DefaultChangeNotiDetailUseCase(notiRepository: notiRepository)
+        let fetchNotiListUseCase = DefaultFetchNotiListUseCase(notiRepository: notiRepository)
+        let viewModel = DiningViewModel(
+            fetchDiningListUseCase: fetchDiningListUseCase,
+            logAnalyticsEventUseCase: logAnalyticsEventUseCase,
+            dateProvder: dateProvider,
+            shareMenuListUseCase: shareMenuListUseCase,
+            diningLikeUseCase: diningLikeUseCase,
+            changeNotiUseCase: changeNotiUseCase,
+            fetchNotiListUsecase: fetchNotiListUseCase,
+            changeNotiDetailUseCase: changeNotiDetailUseCase
+        )
+        let viewController = DiningViewController(viewModel: viewModel)
+        viewController.title = "식단"
+        return viewController
+    }
+    
+    private func makeShopViewController() -> ShopViewController {
+        let shopService = DefaultShopService()
+        let shopRepository = DefaultShopRepository(service: shopService)
+        let fetchShopListUseCase = DefaultFetchShopListUseCase(shopRepository: shopRepository)
+        let fetchEventListUseCase = DefaultFetchEventListUseCase(shopRepository: shopRepository)
+        let fetchShopCategoryListUseCase = DefaultFetchShopCategoryListUseCase(shopRepository: shopRepository)
+        let fetchShopBenefitUseCase = DefaultFetchShopBenefitUseCase(shopRepository: shopRepository)
+        let fetchBeneficialShopUseCase = DefaultFetchBeneficialShopUseCase(shopRepository: shopRepository)
+        let logAnalyticsEventUseCase = DefaultLogAnalyticsEventUseCase(repository: GA4AnalyticsRepository(service: GA4AnalyticsService()))
+        let getUserScreenTimeUseCase = DefaultGetUserScreenTimeUseCase()
+        let viewModel = ShopViewModel(
+            fetchShopListUseCase: fetchShopListUseCase,
+            fetchEventListUseCase: fetchEventListUseCase,
+            fetchShopCategoryListUseCase: fetchShopCategoryListUseCase,
+            fetchShopBenefitUseCase: fetchShopBenefitUseCase,
+            fetchBeneficialShopUseCase: fetchBeneficialShopUseCase,
+            logAnalyticsEventUseCase: logAnalyticsEventUseCase,
+            getUserScreenTimeUseCase: getUserScreenTimeUseCase
+        )
+        let viewController = ShopViewController(viewModel: viewModel)
+        viewController.title = "주변상점"
+        return viewController
     }
 }
 
