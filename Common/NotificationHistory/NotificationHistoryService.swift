740233a9419d321463e953493fa421cae2e993f3
GI JEONG HONG <hgjwilly@gmail.com>
2026-07-10 22:24:56 +0900

    fix: 에러가 버려지지 않도록 변경함

--- Common/NotificationHistory/NotificationHistoryService.swift
+++ Common/NotificationHistory/NotificationHistoryService.swift
@@ -38,7 +38,7 @@ final class DefaultNotificationHistoryService: NotificationHistoryService {
         
         switch record.category {
         case .shop, .dining, .keyword, .chat, .callvan, .callvanChat:
-            try? await MainActor.run { [weak container] in
+            try await MainActor.run { [weak container] in
                 container?.mainContext.insert(record)
                 try container?.mainContext.save()
             }
