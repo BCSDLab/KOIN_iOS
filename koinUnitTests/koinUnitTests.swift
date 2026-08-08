849539ace25825b58a921f723d02b2b478340f65
이은지 <oieunxe@gmail.com>
2026-08-08 15:35:25 +0900

    add: 새로운 Unit Testing 타겟 추가

---
+++ koinUnitTests/koinUnitTests.swift
@@ -0,0 +1,18 @@
+//
+//  koinUnitTests.swift
+//  koinUnitTests
+//
+//  Created by 이은지 on 8/8/26.
+//
+
+import Testing
+
+struct koinUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
